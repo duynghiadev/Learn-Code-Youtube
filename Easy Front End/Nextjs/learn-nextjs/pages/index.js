8a4ad4197cf91b90e6c46e7cf60d55780d499adc
duynghiadev <duynghia22302@gmail.com>
2023-08-30 16:41:39 +0700

    NextJS: 02-03 Navigation between Pages

--- Easy Front End/Nextjs/learn-nextjs/pages/index.js
+++ Easy Front End/Nextjs/learn-nextjs/pages/index.js
@@ -1,8 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import { useRouter } from 'next/dist/client/router'
 
 export default function Home() {
+  const router = useRouter()
+
+  function goToDetailPage() {
+    router.push({
+      pathname: '/posts/[postId]',
+      query: {
+        postId: 123,
+        ref: 'social'
+      }
+    })
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,12 +27,14 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
+          Welcome to <a href='https://nextjs.org'>Duy Nghia Dev</a>
         </h1>
 
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.js</code>
         </p>
+
+        <button onClick={goToDetailPage}>Go to post detail page</button>
 
         <div className={styles.grid}>
           <a href='https://nextjs.org/docs' className={styles.card}>
@@ -44,6 +60,10 @@ export default function Home() {
             <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
+
+        <div style={{ marginTop: '2000px' }}></div>
+
+        <Link href='/about'>Go to about</Link>
       </main>
 
       <footer className={styles.footer}>
