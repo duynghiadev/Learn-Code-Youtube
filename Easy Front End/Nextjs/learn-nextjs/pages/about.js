a64217d367fa90fd9992e9d7f624a086b547a3c1
duynghiadev <duynghia22302@gmail.com>
2023-08-30 18:53:47 +0700

    NextJS: 02-05 Automatic Static Optimization

--- Easy Front End/Nextjs/learn-nextjs/pages/about.js
+++ Easy Front End/Nextjs/learn-nextjs/pages/about.js
@@ -1,5 +1,16 @@
+import { useRouter } from 'next/dist/client/router'
 import React from 'react'
 
 export default function AboutPage() {
+  const router = useRouter()
+
+  console.log('About query: ', router.query)
+
   return <div>About Page</div>
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {} // will be passed to the page component as props
+  }
 }
