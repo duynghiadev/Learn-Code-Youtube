5aa04753181557f0c5db57928ed8e018fcea98cb
duynghiadev <duynghia22302@gmail.com>
2023-09-04 16:58:37 +0700

    #5 TypeScript Types | Khóa Học TypeScript Cơ Bản Cho Người Mới Bắt Đầu

---
+++ HoiDanIT/learn-typescript-1/project/lesson5/lesson5.js
@@ -0,0 +1,5 @@
+const name = "Eric";
+
+console.log("length = ", name.length);
+console.log("upperCase = ", name.toUpperCase());
+console.log("lowerCase = ", name.toLowerCase());
---
+++ HoiDanIT/learn-typescript-1/project/lesson5/lesson5.ts
@@ -0,0 +1,5 @@
+const nameTS1 = "DuyNghia";
+
+console.log("length = ", nameTS1.length);
+console.log("upperCase = ", nameTS1.toUpperCase());
+console.log("lowerCase = ", nameTS1.toLowerCase());
