5ce29e8d028e70cc7d52e60cdbdfed85c310f92f
duynghiadev <duynghia22302@gmail.com>
2023-09-06 16:46:42 +0700

    #6 Type Annotations - Keyword Type | Khóa Học TypeScript Cơ Bản Cho Người Mới Bắt Đầu

---
+++ HoiDanIT/learn-typescript-1/project/lesson6/lesson6.js
@@ -0,0 +1,10 @@
+let count = 1; // OK
+
+count = "name"; // error ?
+
+console.log(">>> check count: ", count);
+
+let names = ["Eric", "Hoi Dan IT", "TypeScript"]; // array chỉ bao gồm string
+names.push(25); // error ?
+
+console.log(">>> check names: ", names);
