9171d029ff0cda7941abb7d2425a42a5ecb59aed
duynghiadev <duynghia22302@gmail.com>
2023-09-07 17:47:45 +0700

    #23 TypeScript While Loop | Khóa Học TypeScript Cơ Bản Cho Người Mới Bắt Đầu

--- HoiDanIT/learn-typescript/chapter3/lesson22/lesson22.ts
+++ HoiDanIT/learn-typescript/chapter3/lesson22/lesson22.ts
@@ -1,15 +1,15 @@
 for (let i = 0; i < 10; i++) {
-  console.log("> i = ", i);
+  console.log("> i = ", i); // print 0 -> 9
 }
 
 let i2 = 0;
 for (; i2 < 10; i2++) {
-  console.log(">> i1 = ", i2);
+  console.log(">> i1 = ", i2); // print 0 -> 9
 }
 
 let i3 = 0;
 for (;;) {
-  console.log(">>> i3 = ", i3);
+  console.log(">>> i3 = ", i3); // print 0
   i3++;
   if (i3 < 10) break;
 }
