6f5be5825546a80fc92fc1d9ae1415ff148cae2d
duynghiadev <duynghia22302@gmail.com>
2023-09-07 13:08:17 +0700

    #15 TypeScript Any Type | Khóa Học TypeScript Cơ Bản Cho Người Mới Bắt Đầu

--- HoiDanIT/learn-typescript-1/project/lesson15/lesson15.js
+++ HoiDanIT/learn-typescript-1/project/lesson15/lesson15.js
@@ -1,5 +1,7 @@
 let name = "eric"; //string
 
+// Khi sử dụng JavaScript thì chúng ta có thể thay đổi giá trị của nó thành cái gì cũng được. Bởi vì nó là kiểu Javascript Dynamic Typing
+
 name = 35; //number
 
 name = false; //boolean
