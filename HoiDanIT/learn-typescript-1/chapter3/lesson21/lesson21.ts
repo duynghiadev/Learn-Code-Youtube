0d3213ac14787c75a207870348d3c3deab84b2da
duynghiadev <duynghia22302@gmail.com>
2023-09-07 16:57:04 +0700

    #20 TypeScript If else | Khóa Học TypeScript Cơ Bản Cho Người Mới Bắt Đầu

---
+++ HoiDanIT/learn-typescript-1/chapter3/lesson21/lesson21.ts
@@ -0,0 +1,16 @@
+const age1: number = 20;
+
+switch (age1) {
+  case 20:
+  case 19:
+    // code block
+    console.log("Đã đi làm");
+    break;
+  case 12:
+    console.log("Đang làm sinh viên");
+    break;
+  default:
+    // code block
+    console.log(">>> Run default");
+    break;
+}
---
+++ HoiDanIT/learn-typescript-1/chapter3/lesson23/lesson23.ts
@@ -0,0 +1,7 @@
+let counter1 = 0;
+
+while (counter1 < 5) {
+  console.log("counter: ", counter1);
+  if (counter1 % 2 === 1) break;
+  counter1++;
+}
