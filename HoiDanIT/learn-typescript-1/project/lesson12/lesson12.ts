a0c462f9339f48b7d31774d4e6d990630b36accd
duynghiadev <duynghia22302@gmail.com>
2023-09-07 11:17:07 +0700

    #12 Array Type | Khóa Học TypeScript Cơ Bản Cho Người Mới Bắt Đầu

--- HoiDanIT/learn-typescript-1/project/lesson12/lesson12.ts
+++ HoiDanIT/learn-typescript-1/project/lesson12/lesson12.ts
@@ -1,11 +1,18 @@
-let test: (string | number)[] = ["hoidanit", "duynghia", 98];
+let test: (string | number | boolean)[] = ["hoidanit", "duynghia", 98, true];
 let test1 = ["hoidanit", "duynghia", 98];
 
 test.push(25);
+test.push(false);
 test1.push(25);
 
 test.push("Kunhi");
 test1.push("Kunhi");
 
-console.log(">>> check array: ", test);
-console.log(">>> check array: ", test1);
+console.log(">>> check typeof test.push: ", typeof test.push);
+console.log("\n>>> check  test.push: ", test.push);
+
+console.log("\n>>> check typeof test: ", typeof test);
+console.log("\n>>> check test: ", test);
+
+console.log("\n>>> check typeof test1: ", typeof test1);
+console.log("\n>>> check test1: ", test1);
