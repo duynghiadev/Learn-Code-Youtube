91f1ec32eda18276120ac7798ba3feb631278100
duynghiadev <duynghia22302@gmail.com>
2023-09-07 15:37:14 +0700

    #17 TypeScript Data Type - Never | Khóa Học TypeScript Cơ Bản Cho Người Mới Bắt Đầu

--- HoiDanIT/learn-typescript-1/project/lesson17/lesson17.ts
+++ HoiDanIT/learn-typescript-1/project/lesson17/lesson17.ts
@@ -2,13 +2,13 @@ function handleException(errorMessage: string): never {
   throw Error(errorMessage);
 }
 
-function runInfinity(): void {
-  //   while (true) {
-  //     console.log("running...");
-  //   }
-}
+handleException("just a test error ✅");
 
-// handleException("just a test error");
-
-let a = handleException("just a test error");
+let a = handleException("just a test error ❌");
 console.log(">> check a= ", a);
+
+function runInfinity(): void {
+  while (true) {
+    console.log("running...");
+  }
+}
