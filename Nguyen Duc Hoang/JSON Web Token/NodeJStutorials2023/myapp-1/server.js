dbb2f8ac71da677d3a760db1ad74539ff955b89c
duynghiadev <duynghia22302@gmail.com>
2023-09-14 04:48:59 +0700

    Xử lý Request/Response trong Express

--- Nguyen Duc Hoang/JSON Web Token/NodeJStutorials2023/myapp-1/server.js
+++ Nguyen Duc Hoang/JSON Web Token/NodeJStutorials2023/myapp-1/server.js
@@ -1,3 +1,20 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+
+dotenv.config() // must have
+const app = express()
+
+const port = process.env.PORT ?? 3000
+
+app.get('/', (req, res) => {
+  res.send('response from root router')
+})
+
+app.listen(port, async () => {
+  console.log(`listening on port http://localhost:${port}`)
+})
+
+/*
 console.log('Hello, this is NodeJS tutorial 2023')
 
 import { sum, multiply, substract } from './calculations.js'
@@ -6,3 +23,4 @@ console.log(`3 plus 2 is: ${sum(3, 2)}`)
 console.log(`3 * 2 is: ${multiply(3, 2)}`)
 console.log(`3 - 2 is: ${substract(3, 2)}`)
 // i file = 1 module
+*/
