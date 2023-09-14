6aabd109a22de5c594b8ec9076bd79729b348d46
duynghiadev <duynghia22302@gmail.com>
2023-09-15 04:14:07 +0700

    Mã hóa mật khẩu với bcrypt

--- Nguyen Duc Hoang/JSON Web Token/NodeJStutorials2023/myapp-1/repositories/user.js
+++ Nguyen Duc Hoang/JSON Web Token/NodeJStutorials2023/myapp-1/repositories/user.js
@@ -1,6 +1,7 @@
 import Exception from '../exceptions/Exception.js'
 import { print, OutputType } from '../helpers/print.js'
 import User from '../models/User.js'
+import bcrypt from 'bcrypt'
 
 const login = async ({ email, password }) => {
   print('login user in user repository, duynghiadev', OutputType.INFORMATION)
@@ -8,13 +9,25 @@ const login = async ({ email, password }) => {
 
 const register = async ({ name, email, password, phoneNumber, address }) => {
   // validation already done
-  try {
-    let existingUser = await User.findOne({ email }).exec()
-    if (!!existingUser) {
-      throw new Exception(Exception.USER_EXIST)
-    }
-    // encrypt password
-  } catch (exception) {}
+  debugger
+  const existingUser = await User.findOne({ email }).exec()
+  if (!!existingUser) {
+    throw new Exception(Exception.USER_EXIST)
+  }
+  // encrypt password, use bcrypt
+  // used for login purpose
+  // const isMatched = await bcrypt.compare(password, existingUser.password)
+  // if(isMatched){}
+  const hashedPassword = await bcrypt.hash(password, parseInt(process.env.SALT_ROUNDS))
+  // insert to db
+  const newUser = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+    phoneNumber,
+    address
+  })
+  return { ...newUser._doc, password: 'Not show' }
   // print(
   //   'register user with: name: ' + name + ' email: ',
   //   email + ' password: ',
