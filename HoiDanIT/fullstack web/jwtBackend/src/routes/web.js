8baf34a249feb08f7ea43015150dbe9ee25a647e
duynghiadev <duynghia22302@gmail.com>
2023-09-24 05:55:44 +0700

    #3. MVC Design Pattern - Áp Dụng Mô Hình MVC | SERIES FULLSTACK - JWT, Node.JS & React

--- HoiDanIT/fullstack web/jwtBackend/src/routes/web.js
+++ HoiDanIT/fullstack web/jwtBackend/src/routes/web.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import homeController from '../controller/homeController'
 
 const router = express.Router()
 
@@ -6,9 +7,9 @@ const router = express.Router()
  * @param {*} app :  express app
  */
 const initWebRoutes = (app) => {
-  router.get('/', (req, res) => {
-    return res.send('Hello World')
-  })
+  router.get('/', homeController.handleHelloWord)
+  router.get('/user', homeController.handleUserPage)
+
   return app.use('/', router)
 }
 
