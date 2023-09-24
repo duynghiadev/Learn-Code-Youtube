930059227010d949fb18083d33a49af0926c64ec
duynghiadev <duynghia22302@gmail.com>
2023-09-24 10:09:07 +0700

    #5.0 Create New Users - CRUD với Node.JS (Create) | SERIES FULLSTACK - JWT, Node.JS & React

--- HoiDanIT/fullstack web/jwtBackend/src/routes/web.js
+++ HoiDanIT/fullstack web/jwtBackend/src/routes/web.js
@@ -9,6 +9,7 @@ const router = express.Router()
 const initWebRoutes = (app) => {
   router.get('/', homeController.handleHelloWord)
   router.get('/user', homeController.handleUserPage)
+  router.post('/users/create-user', homeController.handleCreateNewUser)
 
   return app.use('/', router)
 }
--- HoiDanIT/fullstack web/jwtBackend/src/server.js
+++ HoiDanIT/fullstack web/jwtBackend/src/server.js
@@ -2,12 +2,17 @@ import express from 'express'
 import configViewEngine from './configs/viewEngine'
 import initWebRoutes from './routes/web'
 require('dotenv').config()
+import bodyParser from 'body-parser'
 
 const app = express()
 const PORT = process.env.PORT || 9999
 
 // config view engine
 configViewEngine(app)
+
+// config body-parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // init web routes
 initWebRoutes(app)
