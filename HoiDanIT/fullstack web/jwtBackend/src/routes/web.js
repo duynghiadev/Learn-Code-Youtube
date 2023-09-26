994cedfe9236a212c49aa53ab1de5290e5a6245c
duynghiadev <duynghia22302@gmail.com>
2023-09-27 05:30:04 +0700

    #5.3 Delete Users - CRUD với Node.JS (Delete) | SERIES FULLSTACK - JWT, Node.JS & React

--- HoiDanIT/fullstack web/jwtBackend/src/routes/web.js
+++ HoiDanIT/fullstack web/jwtBackend/src/routes/web.js
@@ -10,6 +10,7 @@ const initWebRoutes = (app) => {
   router.get('/', homeController.handleHelloWord)
   router.get('/user', homeController.handleUserPage)
   router.post('/users/create-user', homeController.handleCreateNewUser)
+  router.post('/delete-user/:id', homeController.handleDeleteUser)
 
   return app.use('/', router)
 }
