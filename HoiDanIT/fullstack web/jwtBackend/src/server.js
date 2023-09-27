19c805b4a1a3fac1079d90fd2bf766fb954ff141
duynghiadev <duynghia22302@gmail.com>
2023-09-27 07:17:50 +0700

    #6.0 ORM & Setup Sequelize - Cài Đặt ORM cho Node.JS | SERIES FULLSTACK - JWT, Node.JS & React

--- HoiDanIT/fullstack web/jwtBackend/src/server.js
+++ HoiDanIT/fullstack web/jwtBackend/src/server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import configViewEngine from './configs/viewEngine'
+import configViewEngine from './config/viewEngine'
 import initWebRoutes from './routes/web'
 require('dotenv').config()
 import bodyParser from 'body-parser'
