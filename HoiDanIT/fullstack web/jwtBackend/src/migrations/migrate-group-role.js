29a5b04a8f09cce2b3d1c0438c0d1fd275cff1e8
duynghiadev <duynghia22302@gmail.com>
2023-09-28 10:17:24 +0700

    #8.3 Create Models ORM - Tạo Models/Tables Cho Dự Án | SERIES FULLSTACK - JWT, Node.JS & React

--- HoiDanIT/fullstack web/jwtBackend/src/migrations/migrate-group-role.js
+++ HoiDanIT/fullstack web/jwtBackend/src/migrations/migrate-group-role.js
@@ -1,32 +1,17 @@
 'use strict'
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('User', {
+    await queryInterface.createTable('Group_Role', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      email: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      username: {
-        type: Sequelize.STRING
-      },
-      address: {
-        type: Sequelize.STRING
-      },
-      sex: {
-        type: Sequelize.STRING
-      },
-      phone: {
-        type: Sequelize.STRING
-      },
       groupId: {
+        type: Sequelize.INTEGER
+      },
+      roleId: {
         type: Sequelize.INTEGER
       },
       createdAt: {
@@ -40,6 +25,6 @@ module.exports = {
     })
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('User')
+    await queryInterface.dropTable('Group_Role')
   }
 }
--- HoiDanIT/fullstack web/jwtBackend/src/models/project.js
+++ HoiDanIT/fullstack web/jwtBackend/src/models/project.js
@@ -1,7 +1,7 @@
 'use strict'
 const { Model, Sequelize } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Project extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,20 +13,17 @@ module.exports = (sequelize, DataTypes) => {
   }
 
   // object relational mapping
-  User.init(
+  Project.init(
     {
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      username: DataTypes.STRING,
-      address: DataTypes.STRING,
-      sex: DataTypes.STRING,
-      phone: DataTypes.STRING,
-      groupId: DataTypes.INTERGER
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      startDate: DataTypes.STRING,
+      customerId: DataTypes.INTERGER
     },
     {
       sequelize,
-      modelName: 'User'
+      modelName: 'Project'
     }
   )
-  return User
+  return Project
 }
