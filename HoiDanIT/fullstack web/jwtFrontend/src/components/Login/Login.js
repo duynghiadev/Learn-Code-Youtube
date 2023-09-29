23d1ed3aeabcfbbb15a91b3f93bca956289a54ff
duynghiadev <duynghia22302@gmail.com>
2023-09-29 17:53:52 +0700

    #9.4.1 Setup Axios - Gọi APIs Từ React với Axios | SERIES FULLSTACK - JWT, Node.JS & React

--- HoiDanIT/fullstack web/jwtFrontend/src/components/Login/Login.js
+++ HoiDanIT/fullstack web/jwtFrontend/src/components/Login/Login.js
@@ -14,11 +14,17 @@ const Login = () => {
         <div className='row px-3 px-sm-0'>
           <div className='content-left col-12 d-none col-sm-7 d-sm-block'>
             <div className='brand'>Duy Nghia Dev</div>
-            <div className='detail'>Duy Nghia Dev helps you connect and share with the people in your life.</div>
+            <div className='detail'>
+              Duy Nghia Dev helps you connect and share with the people in your life.
+            </div>
           </div>
           <div className='content-right green col-sm-5 col-12 d-flex flex-column gap-3 py-3'>
             <div className='brand d-sm-none'>Duy Nghia Dev</div>
-            <input type='text' className='form-control' placeholder='Email address or phone number' />
+            <input
+              type='text'
+              className='form-control'
+              placeholder='Email address or phone number'
+            />
             <input type='password' className='form-control' placeholder='Password' />
             <button className='btn btn-primary'>Login</button>
             <span className='text-center'>
