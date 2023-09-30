a051a74bf3400210f3811f62b6b360885a341cd1
duynghiadev <duynghia22302@gmail.com>
2023-09-30 07:50:50 +0700

    #10.2.1 API Register - Viết API Thêm Mới Users | SERIES FULLSTACK - JWT, Node.JS & React

--- HoiDanIT/fullstack web/jwtBackend/src/controller/apiController.js
+++ HoiDanIT/fullstack web/jwtBackend/src/controller/apiController.js
@@ -1,3 +1,5 @@
+import loginRegisterService from '../service/loginRegisterService'
+
 const testAPI = (req, res) => {
   return res.status(200).json({
     message: 'ok',
@@ -5,8 +7,39 @@ const testAPI = (req, res) => {
   })
 }
 
-const handeRegister = (req, res) => {
-  console.log('me', req.body)
+const handeRegister = async (req, res) => {
+  try {
+    if (!req.body.email || !req.body.phone || !req.body.password) {
+      return res.status(200).json({
+        EM: 'Missing required parameters', // Error Message
+        EC: '1', // Error Code
+        DT: '' // Date
+      })
+    }
+
+    if (req.body.password && req.body.password.length < 4) {
+      return res.status(200).json({
+        EM: 'Your password must have more than 3 letters', // Error Message
+        EC: '1', //Error Code
+        DT: '' //Date
+      })
+    }
+
+    // Service: create user
+    let data = await loginRegisterService.registerNewUser(req.body)
+
+    return res.status(200).json({
+      EM: data.EM, // Error Message
+      EC: data.EC, // Error Code
+      DT: '' // Date
+    })
+  } catch (e) {
+    return res.status(500).json({
+      EM: 'error from server', // Error Message
+      EC: '-1', // Error Code
+      DT: '' // Date
+    })
+  }
 }
 
 module.exports = {
