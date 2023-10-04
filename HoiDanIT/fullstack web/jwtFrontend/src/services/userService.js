770ff8114b91fd3acb6e9e7a5d148e2ee65878e4
duynghiadev <duynghia22302@gmail.com>
2023-10-05 03:52:31 +0700

    #10.6.1 Display List Users - Hiển Thị Danh Sách Người Dùng | SERIES FULLSTACK - JWT, Node.JS & React

--- HoiDanIT/fullstack web/jwtFrontend/src/services/userService.js
+++ HoiDanIT/fullstack web/jwtFrontend/src/services/userService.js
@@ -16,4 +16,8 @@ const loginUser = (valueLogin, password) => {
   })
 }
 
-export { registerNewUser, loginUser }
+const fetchAllUser = () => {
+  return axios.get('http://localhost:8080/api/v1/user/read')
+}
+
+export { registerNewUser, loginUser, fetchAllUser }
