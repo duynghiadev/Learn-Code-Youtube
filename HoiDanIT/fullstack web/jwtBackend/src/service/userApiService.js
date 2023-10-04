c7afab0e738eb44bc55612dad689575ce213e8a6
duynghiadev <duynghia22302@gmail.com>
2023-10-05 05:27:47 +0700

    #10.6.2 Pagination React & Node.JS - Phân Trang List Users | SERIES FULLSTACK - JWT, Node.JS & React

--- HoiDanIT/fullstack web/jwtBackend/src/service/userApiService.js
+++ HoiDanIT/fullstack web/jwtBackend/src/service/userApiService.js
@@ -32,6 +32,36 @@ const getAllUser = async () => {
   }
 }
 
+const getUserWithPagination = async (page, limit) => {
+  try {
+    let offset = (page - 1) * limit
+    const { count, rows } = await db.User.findAndCountAll({
+      offset: offset,
+      limit: limit
+    })
+
+    let totalPages = Math.ceil(count / limit)
+    let data = {
+      totalRows: count,
+      totalPages: totalPages,
+      users: rows
+    }
+
+    return {
+      EM: 'fetch ok',
+      EC: 0,
+      DT: data
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      EM: 'something wrongs with service',
+      EC: 1,
+      DT: []
+    }
+  }
+}
+
 const createNewUser = async (data) => {
   try {
     await db.User.create({})
@@ -70,5 +100,6 @@ module.exports = {
   getAllUser,
   createNewUser,
   updateUser,
-  deleteUser
+  deleteUser,
+  getUserWithPagination
 }
