499071e9caf77435e37fc5872f75c01d3f4fc4de
duynghiadev <duynghia22302@gmail.com>
2023-10-10 14:13:31 +0700

    #11.6 JWT Middleware - Kiểm Tra Quyền Hạn User | SERIES FULLSTACK - JWT, Node.JS & React

--- HoiDanIT/fullstack web/jwtBackend/src/middleware/JWTAction.js
+++ HoiDanIT/fullstack web/jwtBackend/src/middleware/JWTAction.js
@@ -15,18 +15,74 @@ const createJWT = (payload) => {
 
 const verifyToken = (token) => {
   let key = process.env.JWT_SECRET
-  let data = null
+  let decoded = null
 
   try {
-    let decoded = jwt.verify(token, key)
-    data = decoded
+    decoded = jwt.verify(token, key)
   } catch (error) {
     console.log(error)
   }
-  return data
+  return decoded
+}
+
+const checkUserJWT = (req, res, next) => {
+  let cookies = req.cookies
+  if (cookies && cookies.jwt) {
+    let token = cookies.jwt
+    let decoded = verifyToken(token)
+    if (decoded) {
+      req.user = decoded
+      next()
+    } else {
+      return res.status(401).json({
+        EC: -1,
+        DT: '',
+        EM: 'Not authenticated the user'
+      })
+    }
+  } else {
+    return res.status(401).json({
+      EC: -1,
+      DT: '',
+      EM: 'Not authenticated the user'
+    })
+  }
+}
+
+const checkUserPermission = (req, res, next) => {
+  if (req.user) {
+    let email = req.user.email
+    let roles = req.user.groupWithRoles.Roles
+    let currentUrl = req.path
+    if (!roles || roles.length === 0) {
+      return res.status(403).json({
+        EC: -1,
+        DT: '',
+        EM: `You don't have permission to access this resource...`
+      })
+    }
+    let canAccess = roles.some((item) => item.url === currentUrl)
+    if (canAccess === true) {
+      next()
+    } else {
+      return res.status(403).json({
+        EC: -1,
+        DT: '',
+        EM: `You don't have permission to access this resource...`
+      })
+    }
+  } else {
+    return res.status(401).json({
+      EC: -1,
+      DT: '',
+      EM: 'Not authenticated the user'
+    })
+  }
 }
 
 module.exports = {
   createJWT,
-  verifyToken
+  verifyToken,
+  checkUserJWT,
+  checkUserPermission
 }
--- HoiDanIT/fullstack web/jwtFrontend/src/setup/axios.js
+++ HoiDanIT/fullstack web/jwtFrontend/src/setup/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { toast } from 'react-toastify'
 
 // Set config defaults when creating the instance
 const instance = axios.create({
@@ -32,7 +33,47 @@ instance.interceptors.response.use(
   function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    return Promise.reject(error)
+    const status = (error && error.response && error.response.status) || 500
+
+    switch (status) {
+      // authentication (token related issues)
+      case 401: {
+        toast.error('Unauthorized the user. Please login...')
+        // window.location.href = '/login'
+        return Promise.reject(error)
+      }
+
+      // forbidden (permission related issues)
+      case 403: {
+        toast.error(`You don't have permission to access this resource...`)
+        return Promise.reject(error)
+      }
+
+      // bad request
+      case 400: {
+        return Promise.reject(error)
+      }
+
+      // not found
+      case 404: {
+        return Promise.reject(error)
+      }
+
+      // conflict
+      case 409: {
+        return Promise.reject(error)
+      }
+
+      // unprocessable
+      case 422: {
+        return Promise.reject(error)
+      }
+
+      // generic api error (server related) unexpected
+      default: {
+        return Promise.reject(error)
+      }
+    }
   }
 )
 
