73f3c07320983a3d2de63540b3b005395e362d27
duynghiadev <duynghia22302@gmail.com>
2023-10-10 14:48:35 +0700

    #11.7 Middleware Routes - Check Quyền Cho Tất Cả Routes | SERIES FULLSTACK - JWT, Node.JS & React

--- HoiDanIT/fullstack web/jwtBackend/src/routes/api.js
+++ HoiDanIT/fullstack web/jwtBackend/src/routes/api.js
@@ -14,10 +14,12 @@ const initApiRoutes = (app) => {
   // path, handler
   // rest api
   // GET - R, POST - C, PUT - U, DELETE - D
+  router.all('*', checkUserJWT, checkUserPermission)
+
   router.post('/register', apiController.handleRegister)
   router.post('/login', apiController.handleLogin)
 
-  router.get('/user/read', checkUserJWT, checkUserPermission, userController.readFunc)
+  router.get('/user/read', userController.readFunc)
   router.post('/user/create', userController.createFunc)
   router.put('/user/update', userController.updateFunc)
   router.delete('/user/delete', userController.deleteFunc)
