a53738c1fe4ff1e700e3f0bb470a6b3d2690f9f5
duynghiadev <duynghia22302@gmail.com>
2023-10-11 09:14:17 +0700

    #12.1 Design Header - Thiết Kế Navbar Bootstrap 5 | SERIES FULLSTACK - JWT, Node.JS & React

--- HoiDanIT/fullstack web/jwtFrontend/src/App.js
+++ HoiDanIT/fullstack web/jwtFrontend/src/App.js
@@ -1,12 +1,12 @@
 import './App.scss'
-import Nav from './components/Navigation/Nav'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { useContext, useEffect, useState } from 'react'
+import { useContext } from 'react'
 import AppRoutes from './routes/AppRoutes'
 import { Rings } from 'react-loader-spinner'
 import { UserContext } from './context/UserContext'
+import NavHeader from './components/Navigation/Nav'
 
 function App() {
   const { user } = useContext(UserContext)
@@ -14,22 +14,21 @@ function App() {
   return (
     <>
       <Router>
-        {user && user.isLoading
-          ?
-          <div className="loading-container">
+        {user && user.isLoading ? (
+          <div className='loading-container'>
             <Rings height='100' width='100' color='#1877f2' ariaLabel='loading' />
             <div>Loading data...</div>
           </div>
-          :
+        ) : (
           <>
             <div className='app-header'>
-              <Nav />
+              <NavHeader />
             </div>
             <div className='app-container'>
               <AppRoutes />
             </div>
           </>
-        }
+        )}
 
         <ToastContainer
           position='bottom-center'
