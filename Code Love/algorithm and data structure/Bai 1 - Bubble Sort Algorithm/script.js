edb1743d2242468b7473cce4172a65267223ecd5
duynghiadev <duynghia22302@gmail.com>
2023-10-13 10:22:30 +0700

    1. Bubble Sort Algorithm | Thuật toán sắp xếp Bubble cho người mới

--- Code Love/algorithm and data structure/Bai 1 - Bubble Sort Algorithm/script.js
+++ Code Love/algorithm and data structure/Bai 1 - Bubble Sort Algorithm/script.js
@@ -4,13 +4,19 @@ let a = [100,// a[j] -> a[0]
 	7 // a[j+1] a[j]
 ]
 
+let b = [10, 20]
+
 // yeu cau sap xep mang tang dan
+console.log('array length of a:', a.length)
+console.log('array length of b:', b.length)
 
 for (let j = 0; j < a.length - 1; j++) {
 	console.table({
-		j: j, a_j: a[j], a_j_1: a[j + 1]
+		j: j, // 0
+		a_j: a[j], // 100
+		a_j_1: a[j + 1] // 7
 	})
-
+	
 	if (a[j] > a[j + 1]) {
 		let tmp = a[j]
 		a[j] = a[j + 1]
@@ -18,4 +24,20 @@ for (let j = 0; j < a.length - 1; j++) {
 	}
 }
 
-console.log('Table:', a)
+console.log('Table a:', a)
+
+for (let i = 0; i < b.length - 1; i++) {
+	console.table({
+		i: i, // 0
+		b_i: b[i],//10
+		b_i_1: b[i + 1]//20
+	})
+	
+	if (b[i] > b[i + 1]) {
+		let result = b[i]
+		b[i] = b[i + 1]
+		b[i + 1] = result
+	}
+}
+
+console.log('Table b:', b)
