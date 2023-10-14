0700ea7600c41f3a062f03ab6073d8a70afb5765
duynghiadev <duynghia22302@gmail.com>
2023-10-14 14:50:05 +0700

    Test shopping cart

---
+++ Code Love/Learn JavaScript And React/Shopping Cart/Readme.md
@@ -0,0 +1,15 @@
+## Trong series lần này thì chúng ta học về những kiến thức cơ bản mà chúng ta bắt buộc phải biết để làm về reactjs
+
+- Lưu ý: nên học những kiến thức nền tảng cho thật chắc vào thì mới có thể học những thứ nâng cao
+	hơn.
+
+- Đi chậm mà chắc. **Hành trình 1 dặm bước chân đều bắt đầu từ 1 bước chân**
+
+- Cố gắng học một 1 ngôn ngữ (language) thì các ngôn ngữ sau đó chúng ta học rất dễ, chỉ là khác
+	nhau cú pháp thôi
+
+> [Link Tutorial](https://bom.so/Syj17x)
+
+## Trước khi bắt tay vào làm các dự án thì chúng ta nên đi phân tích các yêu cầu của dự án, đây là điều nên làm dành cho các lập trình viên
+
+- 
--- Code Love/Shopping Cart/src/service/cart.js
+++ Code Love/Shopping Cart/src/service/cart.js
@@ -3,7 +3,7 @@ const carts = [
 		id: 1,
 		title: 'Samsung Galaxy S7',
 		price: 10,
-		img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368215/phone-2_ohtt5s.png',
+		img: 'https://by.tn/b2G0',
 		amount: 3
 	},
 	
@@ -11,7 +11,7 @@ const carts = [
 		id: 2,
 		title: 'google pixel ',
 		price: 20,
-		img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png',
+		img: 'https://by.tn/dkCK',
 		amount: 1
 	},
 	
@@ -19,7 +19,7 @@ const carts = [
 		id: 3,
 		title: 'Xiaomi Redmi Note 2',
 		price: 50,
-		img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png',
+		img: 'https://by.tn/iZUq',
 		amount: 1
 	}
 ]
