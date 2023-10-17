5bcf6f4f19535e16762f94320834b771a7f61dbc
duynghiadev <duynghia22302@gmail.com>
2023-10-17 09:27:57 +0700

    Hide Row Cart When Amount Zezo| Shopping Cart #14

--- Code Love/Learn JavaScript And React/Shopping Cart/src/components/CartList.jsx
+++ Code Love/Learn JavaScript And React/Shopping Cart/src/components/CartList.jsx
@@ -5,15 +5,17 @@ function CartList(props) {
 
   function cartRenderList() {
     const cartListRender = carts.map((cartItem) => {
-      return (
-        <CartItem
-          cartItem={cartItem}
-          key={cartItem.id}
-          onClickRemove={onClickRemove}
-          onClickIncrement={onClickIncrement}
-          onClickDecrement={onClickDecrement}
-        />
-      )
+      if (cartItem.amount > 0) {
+        return (
+          <CartItem
+            cartItem={cartItem}
+            key={cartItem.id}
+            onClickRemove={onClickRemove}
+            onClickIncrement={onClickIncrement}
+            onClickDecrement={onClickDecrement}
+          />
+        )
+      }
     })
     return cartListRender
   }
