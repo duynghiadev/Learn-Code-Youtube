704c516d19ca94bb7fea9e4904abd218b7f9e86d
duynghiadev <duynghia22302@gmail.com>
2023-10-18 05:46:47 +0700

    13. Thêm hiện validate input name | Javascript project ecommerce | Js project ecommerce

--- Code Love/Learn JavaScript And React/html_css_start_ecommerce_full_js/assets/js/page/signup.js
+++ Code Love/Learn JavaScript And React/html_css_start_ecommerce_full_js/assets/js/page/signup.js
@@ -1,10 +1,22 @@
 // 1. chọn element
 const btnSignUpSelector = document.querySelector(".btn-signup")
+const inputNameSelector = document.querySelector(".name")
 
 // 2. function xử lý sự kiện + chạy lần đầu khi load
 function handleSignUpClick(event) {
   event.preventDefault()
-  console.log(1)
+  // 1. Thực hiện validate
+  let inputValueName = inputNameSelector.value
+  let errorNameSelector = inputNameSelector
+    .closest(".form-group")
+    .querySelector(".error_message")
+  if (inputValueName === "") {
+    inputNameSelector.classList.add("error")
+    errorNameSelector.textContent = "Tên không được để trống"
+  } else {
+    inputNameSelector.classList.remove(".error")
+    errorNameSelector.textContent = ""
+  }
 }
 
 // 3. Thêm sự kiện cho phần tử
