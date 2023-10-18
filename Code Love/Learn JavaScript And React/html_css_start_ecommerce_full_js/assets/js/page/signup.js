f302ed3122fbac429bb8baffc590cb5a15345877
duynghiadev <duynghia22302@gmail.com>
2023-10-18 09:08:40 +0700

    14. Thêm hiện validate cơ bản các field còn lại| Javascript project ecommerce | Js project ecommerce

--- Code Love/Learn JavaScript And React/html_css_start_ecommerce_full_js/assets/js/page/signup.js
+++ Code Love/Learn JavaScript And React/html_css_start_ecommerce_full_js/assets/js/page/signup.js
@@ -1,23 +1,53 @@
 // 1. chọn element
-const btnSignUpSelector = document.querySelector(".btn-signup")
-const inputNameSelector = document.querySelector(".name")
+const btnSignUpSelector = document.querySelector('.btn-signup')
+const inputNameSelector = document.querySelector('.name')
+const inputEmailSelector = document.querySelector('.email')
+const inputPasswordSelector = document.querySelector('.password')
 
 // 2. function xử lý sự kiện + chạy lần đầu khi load
 function handleSignUpClick(event) {
   event.preventDefault()
   // 1. Thực hiện validate
+
+  // 1.1 Validate cho input name
   let inputValueName = inputNameSelector.value
   let errorNameSelector = inputNameSelector
-    .closest(".form-group")
-    .querySelector(".error_message")
-  if (inputValueName === "") {
-    inputNameSelector.classList.add("error")
-    errorNameSelector.textContent = "Tên không được để trống"
+    .closest('.form-group')
+    .querySelector('.error_message')
+  if (inputValueName === '') {
+    inputNameSelector.classList.add('error')
+    errorNameSelector.textContent = 'Tên không được để trống'
   } else {
-    inputNameSelector.classList.remove(".error")
-    errorNameSelector.textContent = ""
+    inputNameSelector.classList.remove('error')
+    errorNameSelector.textContent = ''
+  }
+
+  // 1.2 Validate cho input email
+  let inputValueEmail = inputEmailSelector.value
+  let errorEmailSelector = inputEmailSelector
+    .closest('.form-group')
+    .querySelector('.error_message')
+  if (inputValueEmail === '') {
+    inputEmailSelector.classList.add('error')
+    errorEmailSelector.textContent = 'Email không được để trống'
+  } else {
+    inputEmailSelector.classList.remove('error')
+    errorEmailSelector.textContent = ''
+  }
+
+  // 1.3 Validate cho input password
+  let inputPasswordValue = inputPasswordSelector.value
+  let errorPassSelector = inputPasswordSelector
+    .closest('.form-group')
+    .querySelector('.error_message')
+  if (inputPasswordValue === '') {
+    inputPasswordSelector.classList.add('error')
+    errorPassSelector.textContent = 'Password không được để trống'
+  } else {
+    inputPasswordSelector.classList.remove('error')
+    errorPassSelector.textContent = ''
   }
 }
 
 // 3. Thêm sự kiện cho phần tử
-btnSignUpSelector.addEventListener("click", handleSignUpClick)
+btnSignUpSelector.addEventListener('click', handleSignUpClick)
