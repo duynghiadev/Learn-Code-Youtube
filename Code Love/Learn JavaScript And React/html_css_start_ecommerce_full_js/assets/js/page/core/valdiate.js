32fd7ffb031bcc1192445b7815e84b88307d51fe
duynghiadev <duynghia22302@gmail.com>
2023-10-24 13:50:49 +0700

    54. Senior validate form như thế nào phần 3 | Javascript project ecommerce | Js project ecommerce

--- Code Love/Learn JavaScript And React/html_css_start_ecommerce_full_js/assets/js/page/core/valdiate.js
+++ Code Love/Learn JavaScript And React/html_css_start_ecommerce_full_js/assets/js/page/core/valdiate.js
@@ -1,3 +1,17 @@
 function Validate(option) {
-  console.log('option:', option)
+  // 1. Lấy ra container bao đóng form
+  const container = document.querySelector(option.container)
+  // 2. Tất cả các elements khác query dựa vào container
+  const btnSignUpSelector = container.querySelector('.btn-signup')
+
+  function initEventAndData() {
+    btnSignUpSelector.addEventListener('click', handleSignUpClick)
+  }
+
+  function handleSignUpClick(event) {
+    event.preventDefault()
+  }
+
+  // add event listener + data init
+  initEventAndData()
 }
