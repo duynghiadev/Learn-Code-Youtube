a6f0db8ae7acaf8d14104cefd4c6499a649114cd
duynghiadev <duynghia22302@gmail.com>
2023-10-25 12:25:55 +0700

    React 101 - #7 Sử dụng uncontrolled components và useRef

--- Komaster/React/lession-7-uncontrolled-components/components/UseRefDemo.jsx
+++ Komaster/React/lession-7-uncontrolled-components/components/UseRefDemo.jsx
@@ -1,23 +1,25 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from 'react'
 
 function UseRefDemo() {
-  const [inputValue, setInputValue] = useState("");
-  const count = useRef(0);
+  const [inputValue, setInputValue] = useState('')
+  const count = useRef(0)
 
   const onChange = (e) => {
-    setInputValue(e.target.value);
-  };
+    setInputValue(e.target.value)
+  }
 
   useEffect(() => {
     count.current += 1
-  });
+  })
 
   return (
-    <div className="use-ref">
-      <input type="text" value={inputValue} onChange={onChange} />
+    <div className='use-ref'>
+      <h1>=========================</h1>
+      <input type='text' value={inputValue} onChange={onChange} />
       <h1>Render Count: {count.current}</h1>
+      <h1>=========================</h1>
     </div>
-  );
+  )
 }
 
-export default UseRefDemo;
+export default UseRefDemo
--- Komaster/React/lession-7-uncontrolled-components/src/App.jsx
+++ Komaster/React/lession-7-uncontrolled-components/src/App.jsx
@@ -1,15 +1,15 @@
-import FormRef from "../components/FormRef";
-import UseRefDemo from "../components/UseRefDemo";
-import PrevState from "../components/PrevState";
+import FormRef from '../components/FormRef'
+import UseRefDemo from '../components/UseRefDemo'
+import PrevState from '../components/PrevState'
 
 function App() {
   return (
-    <div className="container">
-      {/* <FormRef /> */}
-      {/* <UseRefDemo /> */}
+    <div className='container'>
+      <FormRef />
+      <UseRefDemo />
       <PrevState />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
