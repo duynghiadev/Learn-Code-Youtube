ed164c8b622d06b30697592e67963912b842bef8
duynghiadev <duynghia22302@gmail.com>
2023-10-26 03:33:17 +0700

    React 101 - #12 Cách điều hướng sử dụng react-router

--- Komaster/React/lession-12-react-router-dom/src/App.jsx
+++ Komaster/React/lession-12-react-router-dom/src/App.jsx
@@ -1,16 +1,16 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <h1>Bookkeeper</h1>
-      <nav className="main-nav">
-        <Link to="/invoices">Invoices</Link> |{" "}
-        <Link to="/expenses">Expenses</Link>
+      <nav className='main-nav'>
+        <Link to='/invoices'>Invoices</Link> |{' '}
+        <Link to='/expenses'>Expenses</Link>
       </nav>
       <Outlet />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
