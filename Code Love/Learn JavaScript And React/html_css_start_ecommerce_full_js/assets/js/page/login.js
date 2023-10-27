987593232efb9aa34ac7faff9722c666464f10ad
duynghiadev <duynghia22302@gmail.com>
2023-10-28 05:40:57 +0700

    84. Thực hiện validate đăng nhập javascript | Javascript project ecommerce | Js project ecommerce

--- Code Love/Learn JavaScript And React/html_css_start_ecommerce_full_js/assets/js/page/login.js
+++ Code Love/Learn JavaScript And React/html_css_start_ecommerce_full_js/assets/js/page/login.js
@@ -1,3 +1,33 @@
+function rules() {
+  return {
+    email: {
+      required: true,
+      minlength: 3,
+      regex: /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
+    },
+    password: {
+      required: true,
+      minlength: 8
+    }
+  }
+}
+
+function messages() {
+  return {
+    name_required: 'Tên không được để trống',
+    email_required: 'Email không được để trống',
+    email_regex: 'Email không đúng định dạng'
+  }
+}
+
+function validateSuccess() {
+  console.log('validate success')
+}
+
 let loginInstanceValidate = new Validate({
-  container: '#page_login'
+  container: '.login_form',
+  btnClassSubmit: 'btn-login',
+  rules: rules(),
+  messages: messages(),
+  success: validateSuccess
 })
