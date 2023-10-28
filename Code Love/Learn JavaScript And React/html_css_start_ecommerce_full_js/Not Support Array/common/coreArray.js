c4253d913e2dc3210038dc6a074cd53d3a7763a1
duynghiadev <duynghia22302@gmail.com>
2023-10-28 17:25:31 +0700

    89. GÓC NHÌN SENIOR: TƯ DUY KHÁC BIỆT KHI HỌC VỀ CALLBACK(map, some ..) | Javascript project

---
+++ Code Love/Learn JavaScript And React/html_css_start_ecommerce_full_js/Not Support Array/common/coreArray.js
@@ -0,0 +1,21 @@
+function checkOneElementExit(userArray, callback) {
+  for (let i = 0; i < userArray.length; i++) {
+    if (callback(userArray[i])) {
+      return true
+    }
+  }
+  return false
+}
+
+// không chính xác
+// 1. Đảm bảo code mới hoạt động ok
+// 2. Code cũ cũng hoạt động ok
+
+// senior --> nghĩ nhiều --> sau này ít bug nhất --> họ có sửa đổi --> không ảnh hưởng code cũ
+
+// if (email) --> điều kiện của bài toán khác biệt --> hiện tại đang viết trong core common --> nên để người sử dụng thư viện đưa vào
+
+// thực thi hàm if (trong đây là 1 hàm thực thi -> true -> false -> dựa vào điều kiện trong body hàm)
+
+// 1. Kết luận: hàm define điều kiện do người dùng thư viện define ra
+// 2. Thay điều kiện bằng lời gọi hàm
--- Code Love/Learn JavaScript And React/html_css_start_ecommerce_full_js/Not Support Array/script.js
+++ Code Love/Learn JavaScript And React/html_css_start_ecommerce_full_js/Not Support Array/script.js
@@ -1,35 +1,45 @@
 let users = [
-  { name: 'duynghia1', email: 'duynghia1@gmail.com' },
-  { name: 'duynghia2', email: 'duynghia2@gmail.com' },
-  { name: 'duynghia3', email: 'duynghia3@gmail.com' }
-]
-
-// Tư duy học hàm some, every, forEach, map, reducer, filter, find, findIndex
-
-// input --> array
-
-// output --> some
-// 1. Nó sẽ return true nếu 1 callback trả về true
-// 2. Nếu nó chạy hết tất cả các phần tử trong mảng --> không có phần tử nào return true --> return false
-
-// Hàm chung
-function checkOneElementExit(userArray, keyName, value) {
-  for (let i = 0; i < userArray.length; i++) {
-    if (userArray[i][keyName] === value) {
-      return true
+  {
+    name: 'duynghia1',
+    mail: 'duynghia1@gmail.com',
+    address: {
+      city: 'Ha Noi'
+    }
+  },
+  {
+    name: 'duynghia2',
+    mail: 'duynghia2@gmail.com',
+    address: {
+      city: 'Ho Chi Minh'
+    }
+  },
+  {
+    name: 'duynghia3',
+    mail: 'duynghia3@gmail.com',
+    address: {
+      city: 'Da Nang'
     }
   }
-  return false
-}
+]
 
-// Call function
-let isCheckExitName = checkOneElementExit(users, 'name', 'duynghia1')
-let isCheckExitEmail = checkOneElementExit(
-  users,
-  'email',
-  'duynghia1@gmail.com'
-)
+// solid
+// s -> S - Single-responsiblity Principle
+
+// gọi hàm --> sum
+let isCheckExitName = checkOneElementExit(users, function (element) {
+  return element.name === 'duynghia10'
+})
+
+let isCheckExitEmail = checkOneElementExit(users, function (element) {
+  return element.mail === 'duynghia1@gmail.com'
+})
+
+let isCheckExitCity = checkOneElementExit(users, function (element) {
+  return element.address.city === 'Ha Noi'
+})
+
 console.log(isCheckExitName)
 console.log(isCheckExitEmail)
+console.log(isCheckExitCity)
 
 // Junior viết này là oke rồi -> nhưng không -> không ổn đâu -> some -> middle, senior
