fd5d72a6cb34a3642c4f67ad87a65327606d52da
duynghiadev <duynghia22302@gmail.com>
2023-10-29 18:25:01 +0700

    93. Tạo mới danh mục sản phẩm siêu chi tiết | Javascript project ecommerce | Js project ecommerce

--- Code Love/Learn JavaScript And React/html_css_start_ecommerce_full_js/assets/js/page/admin-category.js
+++ Code Love/Learn JavaScript And React/html_css_start_ecommerce_full_js/assets/js/page/admin-category.js
@@ -1,3 +1,19 @@
+function validateSuccess() {
+  // 1. Lấy ra thông tin của danh mục
+  const nameCategory = document.querySelector('.category_name').value
+  // 2. Tạo ra object chứa thông tin danh mục
+  const newCate = {
+    id: crypto.randomUUID(),
+    name: nameCategory
+  }
+  // 3. Đưa object vào trong mảng category
+  let categories = JSON.parse(localStorage.getItem('categories')) || []
+  console.log('categories', categories)
+  categories.push(newCate)
+  // 4. Lưu vào trong localStorage
+  localStorage.setItem('categories', JSON.stringify(categories))
+}
+
 let validateCategory = new Validate({
   container: '#category_form_add',
   btnClassSubmit: 'btn_category_save',
@@ -9,7 +25,5 @@ let validateCategory = new Validate({
   messages: {
     category_name_required: 'Danh mục không được để trống'
   },
-  success: function () {
-    console.log('success')
-  }
+  success: validateSuccess
 })
