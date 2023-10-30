d67b09023a707e99afb1343ac6150a63a759c582
duynghiadev <duynghia22302@gmail.com>
2023-10-30 10:36:06 +0700

    95. Hiển Thị Danh Mục Sản Phẩm Mà Không Load Lại Trang | Javascript project ecommerce | Js project

--- Code Love/Learn JavaScript And React/html_css_start_ecommerce_full_js/assets/js/page/admin-category.js
+++ Code Love/Learn JavaScript And React/html_css_start_ecommerce_full_js/assets/js/page/admin-category.js
@@ -1,3 +1,30 @@
+function showDataCateFromLocal() {
+  // 1. Lấy toàn bộ danh mục trong local
+  const categories = JSON.parse(localStorage.getItem('categories'))
+  // 2. Xây dụng cấu trúc html cho danh mục
+  let htmlResult = ''
+  categories.forEach(function (element) {
+    htmlResult =
+      htmlResult +
+      `<tr>
+          <td>${element.name}</td>
+          <td>
+            <button class="btn_common btn_eidt">
+              Edit
+            </button>
+            <button class="btn_common btn_delete">
+              Delete
+            </button>
+          </td>
+        </tr>`
+  })
+  // 3. Đưa kết quả toàn bộ danh mục tbody của table
+  document.querySelector('.category_table').innerHTML = htmlResult
+}
+
+// Hiển thị dữ liệu category từ local
+showDataCateFromLocal()
+
 function validateSuccess() {
   // 1. Lấy ra thông tin của danh mục
   const nameCategory = document.querySelector('.category_name').value
@@ -11,6 +38,8 @@ function validateSuccess() {
   const categoriesUpdate = [...categories, newCate]
   // 4. Lưu vào trong localStorage
   localStorage.setItem('categories', JSON.stringify(categoriesUpdate))
+  // 5. Hiển thị dữ liệu ngay lập tức khi thêm thành công
+  showDataCateFromLocal()
 }
 
 let validateCategory = new Validate({
