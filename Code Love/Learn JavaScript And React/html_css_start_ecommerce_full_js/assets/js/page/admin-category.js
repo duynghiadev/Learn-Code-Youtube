71a5bb48c54a66e1c90da816bf09725235f8efd3
duynghiadev <duynghia22302@gmail.com>
2023-10-31 17:24:29 +0700

    102. THỰC HIỆN CẬP NHẬT DANH MỤC SẢN PHẨM JAVASCRIPT | Javascript project ecommerce | Js project

--- Code Love/Learn JavaScript And React/html_css_start_ecommerce_full_js/assets/js/page/admin-category.js
+++ Code Love/Learn JavaScript And React/html_css_start_ecommerce_full_js/assets/js/page/admin-category.js
@@ -27,6 +27,43 @@ function showDataCateFromLocal() {
 }
 
 function validateSuccess() {
+  if (buttonSave.classList.contains('update')) {
+    updateCategory()
+  } else {
+    addCategory()
+  }
+}
+
+function updateCategory() {
+  // 1. Lấy ra thông tin của danh mục
+  const nameCategory = categoryInputName.value
+  // 2. Tạo ra dữ liệu update
+  // 2.1 Lấy ra id update
+  const categories = JSON.parse(localStorage.getItem('categories')) || []
+  const idUpdate = buttonSave.getAttribute('data-id')
+  const categoriesUpdate = categories.map(function (element) {
+    if (element.id === idUpdate) {
+      return {
+        id: element.id,
+        name: nameCategory
+      }
+    } else {
+      return element
+    }
+  })
+  // 3. Lưu vào localStorage
+  localStorage.setItem('categories', JSON.stringify(categoriesUpdate))
+  // 4. Hiển thị dữ liệu ngay lập tức khi thêm thành công
+  showDataCateFromLocal()
+  // 5. Reset form
+  categoryInputName.value = ''
+  // 6. Reset form đến trang add category
+  buttonSave.textContent = 'Save'
+  buttonSave.removeAttribute('data-id')
+  buttonSave.classList.remove('update')
+}
+
+function addCategory() {
   // 1. Lấy ra thông tin của danh mục
   const nameCategory = categoryInputName.value
   // 2. Tạo ra object chứa thông tin danh mục
@@ -41,6 +78,8 @@ function validateSuccess() {
   localStorage.setItem('categories', JSON.stringify(categoriesUpdate))
   // 5. Hiển thị dữ liệu ngay lập tức khi thêm thành công
   showDataCateFromLocal()
+  // 6. Reset form
+  categoryInputName.value = ''
 }
 
 function handleProcessData(event) {
