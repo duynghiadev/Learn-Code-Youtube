085eb9f7dc181ec5082402faeffd193c87d8d249
duynghiadev <duynghia22302@gmail.com>
2023-11-02 17:19:31 +0700

    02. Thêm Sửa Xoá JavaScript: Lưu và Đọc dữ liệu từ localStorage.

--- Code Love/Hung Trinh/CRUD JavaScript/script.js
+++ Code Love/Hung Trinh/CRUD JavaScript/script.js
@@ -18,11 +18,49 @@ function addNew() {
   let formElement = document.querySelector('.form')
   let errorElement = formElement.querySelectorAll('.error-message')
   let arrErrorElement = []
+
   for (let i = 0; i < errorElement.length; i++) {
     arrErrorElement.push(errorElement[i].innerText)
   }
+
   let checkErrorElement = arrErrorElement.every((value) => value === '')
   if (checkErrorElement) {
-    // Save code
+    // save data
+    let name = document.getElementById('name').value
+    let address = document.getElementById('address').value
+    let listStudent = localStorage.getItem('list-student')
+      ? JSON.parse(localStorage.getItem('list-student'))
+      : []
+    listStudent.push({
+      name: name,
+      address: address
+    })
+    localStorage.setItem('list-student', JSON.stringify(listStudent))
+    renderStudent()
   }
+}
+
+function renderStudent() {
+  let listStudent = localStorage.getItem('list-student')
+    ? JSON.parse(localStorage.getItem('list-student'))
+    : []
+  let student = `<tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Address</th>
+        <th>Action</th>
+      </tr>`
+
+  listStudent.map((value, index) => {
+    student += `<tr>
+        <td>${index + 1}</td>
+        <td>${value.name}</td>
+        <td>${value.address}</td>
+        <td>
+          <button>Edit</button>
+          <button>Delete</button>
+        </td>
+      </tr>`
+  })
+  document.getElementById('tableContent').innerHTML = student
 }
