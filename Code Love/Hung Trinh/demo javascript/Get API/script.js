a2dc0d9ccd8a3f70312e02376bfc7810307ae72b
duynghiadev <duynghia22302@gmail.com>
2023-11-06 16:53:48 +0700

    02. Chức năng tìm kiếm bằng Javascript.

--- Code Love/Hung Trinh/demo javascript/Get API/script.js
+++ Code Love/Hung Trinh/demo javascript/Get API/script.js
@@ -4,22 +4,30 @@ async function getUser() {
   try {
     let data = await axios.get('https://jsonplaceholder.typicode.com/users')
     users = data.data
-    renderUser()
+    renderUser(users)
   } catch (error) {
     console.log(error)
   }
 }
 
-function renderUser() {
+function renderUser(array) {
   let element = `<tr>
             <th>ID</th>
             <th align='left'>Name</th>
           </tr>`
-  users.map((value) => {
+  array.map((value) => {
     element += `<tr>
               <td>${value.id}</td>
               <td align='center'>${value.name}</td>
             </tr>`
   })
   document.getElementById('table').innerHTML = element
+}
+
+function searchUser() {
+  let valueSearchInput = document.getElementById('search').value
+  let userSearch = users.filter((value) => {
+    return value.name.toUpperCase().includes(valueSearchInput.toUpperCase())
+  })
+  renderUser(userSearch)
 }
