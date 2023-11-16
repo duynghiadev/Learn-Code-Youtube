452e0928156a77ea51592773d67783f74803b695
duynghiadev <duynghia22302@gmail.com>
2023-11-16 18:11:26 +0700

    Tạo Repository Github

---
+++ Code Love/React-2022/13-Create-Repository-Github/public/App.js
@@ -0,0 +1,22 @@
+const reactElement = /*#__PURE__*/ React.createElement(
+  'div',
+  null,
+  /*#__PURE__*/ React.createElement('h1', null, 'duynghiadev'),
+  /*#__PURE__*/ React.createElement('h1', null, 'Hello World'),
+  /*#__PURE__*/ React.createElement(
+    'span',
+    null,
+    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium velit ex magni omnis maiores repudiandae deserunt dignissimos. Eum eveniet in ad! Culpa repellat quod velit veritatis quaerat quasi modi ipsa!'
+  ),
+  /*#__PURE__*/ React.createElement(
+    'span',
+    null,
+    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, officiis magnam? Optio reiciendis quia ullam animi soluta, quaerat fugiat harum, laudantium qui assumenda exercitationem saepe esse, impedit sapiente quae aliquid.'
+  ),
+  /*#__PURE__*/ React.createElement(
+    'b',
+    null,
+    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam similique est quam doloribus, temporibus itaque reiciendis earum consectetur magnam incidunt, perspiciatis error! Itaque nesciunt et quos velit in nam animi.'
+  )
+)
+ReactDOM.render(reactElement, document.getElementById('content'))
