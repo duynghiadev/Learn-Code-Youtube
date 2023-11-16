7cda91056f68a638e4cca2e32a69b529d2e93a47
duynghiadev <duynghia22302@gmail.com>
2023-11-16 17:46:40 +0700

    Export By Name

--- Code Love/React-2022/src/App.jsx
+++ Code Love/React-2022/src/App.jsx
@@ -1,7 +1,20 @@
 import './App.css'
+import * as constantAll from './constant/constant'
+import { city, country } from './constant/constant'
+
+console.log(constantAll) // module
+console.log(constantAll.job)
+console.log(constantAll.name)
+
+console.log(city)
+console.log(country)
 
 function App() {
-  return <>Hello World</>
+  return <div className='App'>Hello World</div>
 }
 
 export default App
+
+// Có 2 cách export:
+// export name: cách này thì này thì export nhiều thứ trong 1 file
+// export default: cách này thì chỉ export function thôi
