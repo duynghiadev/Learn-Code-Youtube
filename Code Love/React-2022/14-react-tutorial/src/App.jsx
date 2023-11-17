e27c5c8dc36fe8f2d63a5120c51df3081707af94
duynghiadev <duynghia22302@gmail.com>
2023-11-17 16:43:42 +0700

    Rendering Classes Dynamically|React Component

--- Code Love/React-2022/14-react-tutorial/src/App.jsx
+++ Code Love/React-2022/14-react-tutorial/src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css'
 import Counter from './component/Counter'
 import Counter1 from './component/Counter1'
+import Wrapping from './component/Warning'
 
 function App() {
-  const urlImage =
-    'https://plus.unsplash.com/premium_photo-1683141316518-70595b251f01?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  // const urlImage =
+  //   'https://plus.unsplash.com/premium_photo-1683141316518-70595b251f01?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
   return (
     <div className='App'>
-      <img className='img_feature' src={urlImage} alt='photo' />
+      <h1>Hello World</h1>
+      {/* <img className='img_feature' src={urlImage} alt='photo' /> */}
       {/* <Counter /> */}
-      <Counter1 />
+      {/* <Counter1 /> */}
+      <Wrapping />
     </div>
   )
 }
