1e9694dcba7b0635ed2c0875dff187d61b6db6c6
duynghiadev <duynghia22302@gmail.com>
2023-11-21 18:32:06 +0700

    Cấu trúc dữ liệu với JavaScript | Giới thiệu về Stack (ngăn xếp) | P. 2

---
+++ LeetCode Challenge Sunday/data structures and algorithms/Bai2-Stack/script.js
@@ -0,0 +1,11 @@
+const stack = [];
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+console.log("stack:", stack);
+
+console.log("Phần tử thứ 1:", stack.pop());
+console.log("Phần tử thứ 2:", stack.pop());
+console.log("Phần tử thứ 3:", stack.pop());
