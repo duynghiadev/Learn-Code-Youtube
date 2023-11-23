fec9c1cc9ad9dda78baa1264eb4fdb26dc5356da
duynghiadev <duynghia22302@gmail.com>
2023-11-24 03:17:46 +0700

    JS Basic - 6 : Lệnh switch case &Toán tử 3 ngôi

---
+++ Code Love/Son Nguyen/Lesson-6/script.js
@@ -0,0 +1,31 @@
+var age_men = 32
+switch (age_men) {
+  case 10: {
+    console.log('age_men', age_men)
+    break
+  }
+  case 20: {
+    console.log('age_men', age_men)
+    break
+  }
+  case 32: {
+    console.log('>> age_men', age_men)
+    break
+  }
+  case 30: {
+    console.log('age_men', age_men)
+    break
+  }
+  case 40: {
+    console.log('age_men', age_men)
+    break
+  }
+  default: {
+    console.log('age_men', age_men)
+    break
+  }
+}
+
+var a = 'duynghiadev'
+varially_son = a === 'deptrai' ? 'chinh xac' : 'sai roi'
+console.log('>> varially_son:', varially_son)
