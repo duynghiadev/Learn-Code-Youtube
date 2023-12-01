890fabf1ee99d5dd9a5dd78aee29f757a2e166af
duynghiadev <duynghia22302@gmail.com>
2023-12-01 15:53:01 +0700

    1. Kiểu Dữ Liệu Nguyên Thuỷ

---
+++ Code Love/Must Kown JavaScript/1-Data-Type/remind.md
@@ -0,0 +1,13 @@
+# Hôm nay chúng ta học bài Kiểu dữ liệu nguyên thủy
+
+- Dưới đây là các kiểu dữ liệu nguyên thủy:
+
+  - undefined
+  - null
+  - numbers
+  - string
+  - boolean
+  - bigInts => Kiểu dữ liệu như này rất ít dùng => mọi người có thể tự research thêm nhé !!!
+  - Symbols => Kiểu dữ liệu như này rất ít dùng => mọi người có thể tự research thêm nhé !!!
+
+- Kiểu dữ liệu object:
---
+++ Code Love/Must Kown JavaScript/1-Data-Type/script.js
@@ -0,0 +1,30 @@
+// Kiểu dữ liệu nguyên thủy
+
+// 1. Kiều dữ liệu undefined
+console.log(typeof undefined)
+let x
+console.log(x)
+
+// 2. Kiểu dữ liệu boolean
+console.log(typeof true)
+console.log(typeof false)
+
+let isTrue = true
+let isFalse = false
+console.log(typeof isTrue)
+console.log(typeof isFalse)
+
+// 3. Kiểu dữ liệu number
+console.log(typeof 123)
+console.log(typeof 123.45)
+
+let numberInt = 123
+let numberFloat = 123.45
+console.log(typeof numberInt)
+console.log(typeof numberFloat)
+
+// 4. Kiểu dữ liệu string
+console.log(typeof 'duynghiadev')
+
+// 5. Kiểu null: kiểu này nó rất đặt biệt
+console.log(typeof null)
