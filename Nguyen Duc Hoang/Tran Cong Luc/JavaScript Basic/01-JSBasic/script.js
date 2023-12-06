81bad6030b97b016767c08b84b0cf632bad0a421
duynghiadev <duynghia22302@gmail.com>
2023-12-07 00:54:46 +0700

    Javascript Cơ Bản - #01 - Biến và kiểu dữ liệu. Solution Challenge 01 (Phần 03)

---
+++ Nguyen Duc Hoang/Tran Cong Luc/JavaScript Basic/01-JSBasic/script.js
@@ -0,0 +1,15 @@
+var firstName = 'duynghiadev'
+var secondName = 'duynghiabuck'
+let age = 18
+console.log(firstName, secondName, age)
+
+// Kiểu này là kiểu dữ liệu luận lý
+let isMarried
+isMarried = false
+console.log(isMarried)
+
+// Toán tử
+/**
+ * /: Đây là phép toán chia lấy kết quả
+ * %: Đây là phép toán chia lấy dư
+ */
