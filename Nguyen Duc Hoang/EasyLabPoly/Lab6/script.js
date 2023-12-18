d77a789d4f87e60ac68530002280904dea957bf5
duynghiadev <duynghia22302@gmail.com>
2023-12-18 20:37:04 +0700

    [WEB1042 - Javascript] Lab 6 Bài 2: Tạo slide show bằng html và javascript (Source Code Download) ✔

--- Nguyen Duc Hoang/EasyLabPoly/Lab6/script.js
+++ Nguyen Duc Hoang/EasyLabPoly/Lab6/script.js
@@ -16,3 +16,40 @@ function fout() {
   let anh = document.getElementById('anh')
   anh.src = out.src
 }
+
+let images = []
+for (let i = 0; i < images.length; i++) {
+  images[i] = new Image()
+  images[i].src = 'images/' + i + '.jpg'
+}
+
+let index = 0
+function first() {
+  index = 0
+  let anh_1 = document.getElementById('anh')
+  anh_1.src = images[index].src
+}
+
+function last() {
+  index = images.length - 1
+  let anh_1 = document.getElementById('anh')
+  anh_1.src = images[index].src
+}
+
+function next() {
+  index++
+  if (index >= images.length) {
+    index = 0
+  }
+  let anh_1 = document.getElementById('anh')
+  anh_1.src = images[index].src
+}
+
+function prev() {
+  index--
+  if (index < 0) {
+    index = images.length - 1
+  }
+  let anh_1 = document.getElementById('anh')
+  anh_1.src = images[index].src
+}
