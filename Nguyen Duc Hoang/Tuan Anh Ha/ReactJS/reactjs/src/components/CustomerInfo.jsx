4f2c6454b4b8827ac98bb8c61d3f303967ec10d0
duynghiadev <duynghia22302@gmail.com>
2023-12-25 18:32:10 +0700

    React Js 2022: Sử dụng Props với Destructuring

--- Nguyen Duc Hoang/Tuan Anh Ha/ReactJS/reactjs/src/components/CustomerInfo.jsx
+++ Nguyen Duc Hoang/Tuan Anh Ha/ReactJS/reactjs/src/components/CustomerInfo.jsx
@@ -1,10 +1,14 @@
 const CustomerInfo = (props) => {
+  const { info } = props // Destructuring
+
   return (
     <div>
       <h3>Customer Info</h3>
       <p>I am a child of Customer Two Component</p>
-      <p>My name is: {props.name}.</p>
-      <p>My email is: {props.email}.</p>
+      <p>My name is: {info.name}.</p>
+      <p>My email is: {info.email}.</p>
+      <p>I live in: {info.location}.</p>
+      <p>I work for: {info.company}.</p>
     </div>
   )
 }
