2d2a22f034d55f621a2580a9a9217f6344c5d260
duynghiadev <duynghia22302@gmail.com>
2023-12-25 10:48:51 +0700

    Batching trong React 18

--- Nguyen Duc Hoang/Tuan Anh Ha/ReactJS/reactjs/src/App.jsx
+++ Nguyen Duc Hoang/Tuan Anh Ha/ReactJS/reactjs/src/App.jsx
@@ -1,7 +1,30 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  return <>Hello World</>
+  const [count, setCount] = useState(0)
+  const [textColor, setTextColor] = useState('black')
+
+  const handleAdd = () => {
+    setCount((count) => count + 1) // not render
+    setTextColor('blue') // not render
+    // Batching: only re-render once at the end
+  }
+
+  const handleSubtract = () => {
+    setCount((count) => count - 1)
+    setTextColor('red')
+  }
+
+  return (
+    <div className='App'>
+      <h1>Batching in React 18</h1>
+      <button onClick={handleAdd}>+ 1</button>
+      <hr />
+      <button onClick={handleSubtract}>- 1</button>
+      <h2 style={{ color: textColor }}>Number: ${count} </h2>
+    </div>
+  )
 }
 
 export default App
