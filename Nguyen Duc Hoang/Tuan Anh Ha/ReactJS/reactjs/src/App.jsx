16c80da33022f20a22cdce3782a590b4968eae7d
duynghiadev <duynghia22302@gmail.com>
2023-12-25 16:24:43 +0700

    React Js 2022: Tạo Components đầu tiên

--- Nguyen Duc Hoang/Tuan Anh Ha/ReactJS/reactjs/src/App.jsx
+++ Nguyen Duc Hoang/Tuan Anh Ha/ReactJS/reactjs/src/App.jsx
@@ -28,7 +28,6 @@ function App() {
 
       <hr />
       <CustomerOne />
-      <hr />
       <CustomerTwo />
     </div>
   )
--- Nguyen Duc Hoang/Tuan Anh Ha/ReactJS/reactjs/src/components/CustomerTwo.jsx
+++ Nguyen Duc Hoang/Tuan Anh Ha/ReactJS/reactjs/src/components/CustomerTwo.jsx
@@ -1,8 +1,11 @@
+import CustomerInfo from './CustomerInfo'
+
 const CustomerTwo = () => {
   return (
     <div>
       <h2>Customer Two</h2>
       <p>I am a function component</p>
+      <CustomerInfo />
     </div>
   )
 }
