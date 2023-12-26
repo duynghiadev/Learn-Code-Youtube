32be3d18e8d226b8c4ef894d3e20820724812396
duynghiadev <duynghia22302@gmail.com>
2023-12-26 09:52:52 +0700

    React Js 2022: Sử dụng Props với Destructuring

--- Nguyen Duc Hoang/Tuan Anh Ha/ReactJS/principle/src/App.jsx
+++ Nguyen Duc Hoang/Tuan Anh Ha/ReactJS/principle/src/App.jsx
@@ -3,6 +3,7 @@ import ComplexClassComponent from './components/componentFirst/classComponentOne
 import ComplexLifecycleComponent from './components/componentFirst/classComponentTwo'
 import ComplexFunctionComponent from './components/componentFirst/functionComponent'
 import AppComponent from './components/props/App Component One/appComponent'
+import UserList from './components/props/App Component Three/appComponent-2'
 import AppComponentOne from './components/props/App Component Two/appComponent-1'
 import ReactBatchingOne from './components/react-batching/ReactBatchingOne'
 import ReactBatchingThree from './components/react-batching/ReactBatchingThree'
@@ -20,7 +21,8 @@ function App() {
       {/* <ComplexLifecycleComponent /> */}
       {/* <ComplexFunctionComponent /> */}
       {/* <AppComponent /> */}
-      <AppComponentOne />
+      {/* <AppComponentOne /> */}
+      <UserList />
     </h2>
   )
 }
