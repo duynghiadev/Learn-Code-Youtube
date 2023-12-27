4506a5698c62fef6a2ef86d523320d63620cc509
duynghiadev <duynghia22302@gmail.com>
2023-12-27 11:52:44 +0700

    React Js 2022: 2 Events (onChange, onSubmit) để xử lý Form

--- Nguyen Duc Hoang/Tuan Anh Ha/ReactJS/reactjs/src/components/Customer/SignupForm.jsx
+++ Nguyen Duc Hoang/Tuan Anh Ha/ReactJS/reactjs/src/components/Customer/SignupForm.jsx
@@ -1,8 +1,23 @@
 const SignupForm = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault() // not reloading the web
+    alert('Thank you for signing up...')
+  }
+
+  const handleChange = (event) => {
+    event.preventDefault() // not reloading the web
+    console.log('handle change...')
+  }
+
   return (
     <div>
       <h3>Sign up Form Now</h3>
       <p>Please sign up to become the member</p>
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input type='text' onChange={handleChange} />
+        <input type='submit' value='Sign up' />
+      </form>
     </div>
   )
 }
