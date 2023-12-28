0a3423e8e3138644c8a4d0dd4ab90578b2ff9a1b
duynghiadev <duynghia22302@gmail.com>
2023-12-28 14:48:58 +0700

    React Js 2022: Hướng dẫn filter( ) trong React

--- Nguyen Duc Hoang/Tuan Anh Ha/ReactJS/reactjs/src/components/Customer/User.jsx
+++ Nguyen Duc Hoang/Tuan Anh Ha/ReactJS/reactjs/src/components/Customer/User.jsx
@@ -1,8 +1,9 @@
-const User = ({ name, email }) => {
+const User = ({ id, name, email, handleDelete }) => {
   return (
     <div>
       <h3>
         {name} - {email}
+        <button onClick={() => handleDelete(id)}>Delete</button>
       </h3>
     </div>
   )
