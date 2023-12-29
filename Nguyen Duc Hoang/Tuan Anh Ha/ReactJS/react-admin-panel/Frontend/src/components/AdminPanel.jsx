56bf0270eadbad1ea80ab7bbcfffced957c7643b
duynghiadev <duynghia22302@gmail.com>
2023-12-30 04:43:52 +0700

    React 2022: Hướng dẫn tạo bảng điều khiển bằng react-admin

---
+++ Nguyen Duc Hoang/Tuan Anh Ha/ReactJS/react-admin-panel/Frontend/src/components/AdminPanel.jsx
@@ -0,0 +1,10 @@
+import { Card, CardContent, CardHeader } from '@mui/material'
+
+const AdminPanel = () => (
+  <Card>
+    <CardHeader title='Welcome to Admin Panel' />
+    <CardContent>This is the board to control all the products and users</CardContent>
+  </Card>
+)
+
+export default AdminPanel
