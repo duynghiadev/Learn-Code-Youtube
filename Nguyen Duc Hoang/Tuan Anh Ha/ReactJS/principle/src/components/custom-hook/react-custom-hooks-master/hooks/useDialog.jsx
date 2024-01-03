9be6aff5f7bc71b8e1ce9803d72e53ccbc487f00
duynghiadev <duynghia22302@gmail.com>
2024-01-03 13:38:40 +0700

    feat: add Application component use Higher Order Component

--- Nguyen Duc Hoang/Tuan Anh Ha/ReactJS/principle/src/components/custom-hook/react-custom-hooks-master/hooks/useDialog.jsx
+++ Nguyen Duc Hoang/Tuan Anh Ha/ReactJS/principle/src/components/custom-hook/react-custom-hooks-master/hooks/useDialog.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 
-
 /**
  * @function useDialogHook
  * @description React Hook to handle open and close dialogs
@@ -8,16 +7,16 @@ import React, { useState } from 'react'
  * @author Sarat Chandra Ejjapureddi
  */
 function useDialogHook() {
-    const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false)
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
 
-    const handleClose = () => {
-        setOpen(false);
-    };
-    return [open, handleClickOpen, handleClose]
+  const handleClose = () => {
+    setOpen(false)
+  }
+  return [open, handleClickOpen, handleClose]
 }
 
 export default useDialogHook
