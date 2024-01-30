29ccaf940401baca6400218ef3e69a77df2a2aae
duynghiadev <duynghia22302@gmail.com>
2024-01-30 13:23:09 +0700

    #3 - Nextjs cơ bản - Thiết lập Pages và Routing

---
+++ Nguyen Duc Hoang/Tu Tran/Next/my-app/pages/about.js
@@ -0,0 +1,17 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus facere cumque ullam.
+        Molestias repellendus itaque nostrum cum sed quasi, omnis error odio, ad asperiores placeat
+        in totam dolore, tempore possimus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia repellendus esse magni, modi
+        tempora fugiat explicabo mollitia enim earum voluptatem similique ipsam velit quas error,
+        sit ab ex nisi minus.
+      </p>
+    </div>
+  )
+}
---
+++ Nguyen Duc Hoang/Tu Tran/Next/my-app/pages/coders/index.js
@@ -0,0 +1,7 @@
+export default function AllCoders() {
+  return (
+    <div>
+      <h1>All Coders</h1>
+    </div>
+  )
+}
