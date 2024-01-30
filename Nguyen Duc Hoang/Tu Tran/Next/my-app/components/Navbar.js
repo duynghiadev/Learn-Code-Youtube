272af9d9b350d88fb04aa7e2bf9526496f1fcb98
duynghiadev <duynghia22302@gmail.com>
2024-01-30 13:32:16 +0700

    #4 - Nextjs cơ bản - Tạo Navbar và Footer component

---
+++ Nguyen Duc Hoang/Tu Tran/Next/my-app/components/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        <a href='#'>Coder Wikipedia</a>
+      </div>
+      <div>
+        <a href='#'>About</a>
+        <a href='#'>All Coders</a>
+      </div>
+    </nav>
+  )
+}
--- Nguyen Duc Hoang/Tu Tran/Next/my-app/pages/index.js
+++ Nguyen Duc Hoang/Tu Tran/Next/my-app/pages/index.js
@@ -1,20 +1,24 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Navbar from '../components/navbar'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam qui nostrum hic sequi labore,
-        eligendi facere fugit unde, at accusantium atque accusamus ea quibusdam minima, ex expedita
-        dolorum distinctio laboriosam.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel optio, blanditiis est modi
-        asperiores illum consequuntur numquam. Excepturi vitae earum aliquid! Ea, quo? Tempore optio
-        itaque sit quae. Nihil, mollitia.
-      </p>
-    </div>
+    <>
+      <Navbar />
+      <div>
+        <h1>Home Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam qui nostrum hic sequi
+          labore, eligendi facere fugit unde, at accusantium atque accusamus ea quibusdam minima, ex
+          expedita dolorum distinctio laboriosam.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel optio, blanditiis est modi
+          asperiores illum consequuntur numquam. Excepturi vitae earum aliquid! Ea, quo? Tempore
+          optio itaque sit quae. Nihil, mollitia.
+        </p>
+      </div>
+      <Footer />
+    </>
   )
 }
