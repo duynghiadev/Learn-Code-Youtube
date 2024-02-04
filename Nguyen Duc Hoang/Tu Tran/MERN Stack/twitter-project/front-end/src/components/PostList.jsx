2ebaad338c53a12d96c27e957caf4e560383383c
duynghiadev <duynghia22302@gmail.com>
2024-02-04 15:16:48 +0700

    #33 - MERN Stack - Cài đặt Axios, Thiết lập proxy+reset, Test

--- Nguyen Duc Hoang/Tu Tran/MERN Stack/twitter-project/front-end/src/components/PostList.jsx
+++ Nguyen Duc Hoang/Tu Tran/MERN Stack/twitter-project/front-end/src/components/PostList.jsx
@@ -1,7 +1,29 @@
+import { useCallback, useEffect } from 'react'
 import '../css/Post.css'
 import PostItem from './PostItem'
+import axios from 'axios'
 
 const PostList = () => {
+  const getAllPosts = useCallback(async () => {
+    try {
+      const option = {
+        method: 'get',
+        url: '/api/v1/posts'
+      }
+      const response = await axios(option)
+      console.log('response:', response)
+
+      const posts = response.data.data.posts
+      console.log('posts:', posts)
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
+
+  useEffect(() => {
+    getAllPosts()
+  }, [getAllPosts])
+
   return (
     // Post List
     <section className='post-section'>
