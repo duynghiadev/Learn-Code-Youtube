a7df0f7becb6b424e21f55684ee176455bde1e28
duynghiadev <duynghia22302@gmail.com>
2024-02-04 13:19:54 +0700

    #31 - MERN Stack - Tạo InitialState và Thiết Lập AppReducer

--- Nguyen Duc Hoang/Tu Tran/MERN Stack/twitter-project/front-end/src/App.jsx
+++ Nguyen Duc Hoang/Tu Tran/MERN Stack/twitter-project/front-end/src/App.jsx
@@ -1,11 +1,16 @@
+import { useReducer } from 'react'
 import PageNotFound from './components/404-page'
 import Header from './components/Header'
 import Login from './components/Login'
 import Main from './components/Main'
 import Register from './components/Register'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import AppReducer from './reducers/AppReducer'
 
 function App() {
+  const initialState = { user: null, posts: [] }
+  const [state, dispatch] = useReducer(AppReducer, initialState)
+
   return (
     <Router>
       <div className='container'>
