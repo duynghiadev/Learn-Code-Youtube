a365b82798d8dc1761767fc37d6df7daa30e65b9
duynghiadev <duynghia22302@gmail.com>
2024-02-05 16:06:57 +0700

    #38 - MERN Stack - PostList và PostItem components

--- Nguyen Duc Hoang/Tu Tran/MERN Stack/twitter-project/front-end/src/components/PostItem.jsx
+++ Nguyen Duc Hoang/Tu Tran/MERN Stack/twitter-project/front-end/src/components/PostItem.jsx
@@ -1,49 +1,72 @@
-const PostItem = () => {
+import { useState } from 'react'
+
+const PostItem = ({ post }) => {
+  const [openEditForm, setOpenEditForm] = useState(false)
+  const [openDeleteConfirm, setOpenDeleteConfirm] = useState(false)
+
+  let date = new Date(post.createdAt)
+
   return (
     // Post Item
     <div className='post-item'>
-      <p className='post-content'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate tenetur qui alias
-        excepturi impedit in eius, hic sed dolorem ipsum, quisquam illum quas quaerat, fugit laborum
-        officia cumque quae sit?
-      </p>
+      <p className='post-content'>{post.content}</p>
       <div className='post-footer'>
         <div className='post-infors'>
-          <span>by Duy Nghia Dev</span>
-          <span>Date: 3/2/2024</span>
+          <span>{post.author.name}</span>
+          <span>
+            Date:
+            {`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`}
+          </span>
         </div>
-        <div className='post-edit-delete'>
-          <span>Edit</span>
-          <span>Delete</span>
-          <span className='delete-question'>Are you sure?</span>
-          <span>Yes</span>
-          <span>No</span>
-        </div>
-      </div>
 
-      <div className='post-edit-form'>
-        <form className='edit-form'>
-          <textarea
-            type='text'
-            name='content'
-            id='content'
-            className='content'
-            placeholder="What's happening?"
-            defaultValue='Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit molestias, repudiandae id
-            rem libero molestiae dolore velit temporibus animi officiis aliquid nemo! In ex fuga
-            quos quasi vitae ullam tempore.'
-          />
-
-          <div className='btn-container'>
-            <button className='btn' type='button'>
-              Update
-            </button>
-            <button className='btn' type='button'>
-              Cancel
-            </button>
+        {post.isEditable && (
+          <div className='post-edit-delete'>
+            {openDeleteConfirm ? (
+              <>
+                <span className='delete-question'>Are you sure?</span>
+                <span>Yes</span>
+                <span onClick={() => setOpenDeleteConfirm(false)}>No</span>
+              </>
+            ) : (
+              <>
+                <span onClick={() => setOpenEditForm(true)}>Edit</span>
+                <span onClick={() => setOpenDeleteConfirm(true)}>Delete</span>
+              </>
+            )}
           </div>
-        </form>
+        )}
       </div>
+
+      {openEditForm && (
+        <div className='post-edit-form'>
+          <form className='edit-form'>
+            <textarea
+              type='text'
+              name='content'
+              id='content'
+              className='content'
+              placeholder="What's happening?"
+              defaultValue='Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit molestias, repudiandae id rem libero molestiae dolore velit temporibus animi officiis aliquid nemo! In ex fuga quos quasi vitae ullam tempore.'
+            />
+
+            <div className='btn-container'>
+              <button
+                className='btn'
+                type='button'
+              >
+                Update
+              </button>
+              <button
+                className='btn'
+                type='button'
+                onClick={() => setOpenEditForm(false)}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   )
 }
--- Nguyen Duc Hoang/Tu Tran/MERN Stack/twitter-project/front-end/src/components/PostList.jsx
+++ Nguyen Duc Hoang/Tu Tran/MERN Stack/twitter-project/front-end/src/components/PostList.jsx
@@ -1,9 +1,12 @@
-import { useCallback, useEffect } from 'react'
+import { useCallback, useContext, useEffect } from 'react'
 import '../css/Post.css'
 import PostItem from './PostItem'
 import axios from 'axios'
+import AppContext from './AppContext'
 
 const PostList = () => {
+  const { state, dispatch } = useContext(AppContext)
+  const { posts, user } = state
   const getAllPosts = useCallback(async () => {
     try {
       const option = {
@@ -15,20 +18,39 @@ const PostList = () => {
 
       const posts = response.data.data.posts
       console.log('posts:', posts)
+
+      dispatch({ type: 'GET_ALL_POSTS', payload: posts })
     } catch (error) {
       console.log(error)
     }
-  }, [])
+  }, [dispatch])
 
   useEffect(() => {
     getAllPosts()
   }, [getAllPosts])
 
+  const newPosts = posts.map((post) => {
+    if (user) {
+      return post.author.name === user.userName
+        ? { ...post, isEditable: true }
+        : post
+    } else {
+      return { ...post, isEditable: false }
+    }
+  })
+
+  console.log('newPosts:', newPosts)
+
   return (
     // Post List
     <section className='post-section'>
       <div className='post-list'>
-        <PostItem />
+        {newPosts.map((post) => (
+          <PostItem
+            post={post}
+            key={post._id}
+          />
+        ))}
       </div>
     </section>
   )
