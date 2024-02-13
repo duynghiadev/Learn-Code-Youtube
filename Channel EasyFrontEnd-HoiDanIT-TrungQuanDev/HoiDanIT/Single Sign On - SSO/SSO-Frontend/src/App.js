1a0c4feb495eb179ba68401fca0f5ddc16bc8bc8
duynghiadev <duynghia22302@gmail.com>
2024-02-13 14:54:24 +0700

    #13 Cài Đặt Thư Viện Cần Thiết | Khóa Học Single Sign On Basic (SSO) với Hỏi Dân IT

--- Channel EasyFrontEnd-HoiDanIT-TrungQuanDev/HoiDanIT/Single Sign On - SSO/SSO-Frontend/src/App.js
+++ Channel EasyFrontEnd-HoiDanIT-TrungQuanDev/HoiDanIT/Single Sign On - SSO/SSO-Frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg'
-import './App.css'
+import About from './components/About/About'
+import Header from './components/Header/Header'
+import { Routes, Route } from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img
-          src={logo}
-          className='App-logo'
-          alt='logo'
-        />
-        <p>Hello World</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route
+          path='/about'
+          element={<About />}
+        ></Route>
+      </Routes>
     </div>
   )
 }
