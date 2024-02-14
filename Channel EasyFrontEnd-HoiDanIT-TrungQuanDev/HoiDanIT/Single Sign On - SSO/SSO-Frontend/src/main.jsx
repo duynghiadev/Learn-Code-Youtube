9f85195a7cdde48df4c8b6a4840b9a0d044178a1
duynghiadev <duynghia22302@gmail.com>
2024-02-14 12:14:24 +0700

    #15 Cài Đặt Thư Viện Redux Cho dự án React | Khóa Học Single Sign On Basic (SSO) với Hỏi Dân IT

--- Channel EasyFrontEnd-HoiDanIT-TrungQuanDev/HoiDanIT/Single Sign On - SSO/SSO-Frontend/src/main.jsx
+++ Channel EasyFrontEnd-HoiDanIT-TrungQuanDev/HoiDanIT/Single Sign On - SSO/SSO-Frontend/src/main.jsx
@@ -5,15 +5,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AppRoute from './routes/AppRoute.jsx'
 import About from './components/About/About.jsx'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<AppRoute />}>
-        <Route path='/about' element={<About />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
-  // </React.StrictMode>,
+  <Provider store={store}>
+    {/* <React.StrictMode> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AppRoute />}>
+          <Route path='/about' element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    {/* </React.StrictMode>, */}
+  </Provider>
 )
