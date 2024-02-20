ef98baecb2d9238c67476d47b27b4bf9b53fdcc0
duynghiadev <duynghia22302@gmail.com>
2024-02-20 11:13:24 +0700

    #9 Setup Redux - Tích Hợp Redux Vào Dự Án React | Khóa Học Redux Ultimate (Redux & Redux Toolkit)

---
+++ Channel EasyFrontEnd-HoiDanIT-TrungQuanDev/HoiDanIT/Redux Ultimate/src/reducer/rootReducer.jsx
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import counterReducer from './counterReducer'
+
+const rootReducer = combineReducers({
+  counter: counterReducer
+})
+
+export default rootReducer
