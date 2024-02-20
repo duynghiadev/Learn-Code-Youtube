02b292d613c21d4091784e08ef7d6bb3a053bcc8
duynghiadev <duynghia22302@gmail.com>
2024-02-20 12:39:51 +0700

    #10 Redux Store | Khóa Học Redux Ultimate (Redux & Redux Toolkit)

--- Channel EasyFrontEnd-HoiDanIT-TrungQuanDev/HoiDanIT/Redux Ultimate/src/redux/store.jsx
+++ Channel EasyFrontEnd-HoiDanIT-TrungQuanDev/HoiDanIT/Redux Ultimate/src/redux/store.jsx
@@ -2,6 +2,8 @@ import { createStore } from 'redux'
 import rootReducer from '../reducer/rootReducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
+// includes: reducers, middleware,...
+
 const store = createStore(rootReducer, composeWithDevTools())
 
 export default store
