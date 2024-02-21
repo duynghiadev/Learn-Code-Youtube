4f5ee7b864979039a591dec39b4ee2b45b5ac413
duynghiadev <duynghia22302@gmail.com>
2024-02-22 05:55:29 +0700

    #19 Setup Redux Thunk - Xử Lý Redux Fetching Data | Khóa Học Redux Ultimate

--- Channel EasyFrontEnd-HoiDanIT-TrungQuanDev/HoiDanIT/Redux Ultimate/src/redux/store.jsx
+++ Channel EasyFrontEnd-HoiDanIT-TrungQuanDev/HoiDanIT/Redux Ultimate/src/redux/store.jsx
@@ -1,9 +1,10 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducer/rootReducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import thunkMiddleware from 'redux-thunk'
 
 // includes: reducers, middleware,...
 
-const store = createStore(rootReducer, composeWithDevTools())
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunkMiddleware)))
 
 export default store
