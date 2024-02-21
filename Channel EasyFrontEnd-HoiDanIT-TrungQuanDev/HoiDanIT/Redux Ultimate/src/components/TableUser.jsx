5f584d1b19baa88b5792609d56bdd608527e99f6
duynghiadev <duynghia22302@gmail.com>
2024-02-22 05:24:57 +0700

    #17 Design Layout React với Bootstrap 5 | Khóa Học Redux Ultimate

---
+++ Channel EasyFrontEnd-HoiDanIT-TrungQuanDev/HoiDanIT/Redux Ultimate/src/components/TableUser.jsx
@@ -0,0 +1,41 @@
+import Table from 'react-bootstrap/Table'
+import Container from 'react-bootstrap/Container'
+
+const TableUser = (props) => {
+  return (
+    <Container>
+      <hr />
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
+  )
+}
+
+export default TableUser
