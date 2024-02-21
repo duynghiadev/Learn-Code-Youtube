b7b474198a2f163dc3236df3ea75ece55aa6361b
duynghiadev <duynghia22302@gmail.com>
2024-02-21 11:21:14 +0700

    Bai 1 - Two Sum

---
+++ LeetCode And DuyNghia Front End/LeetCode Challenge Sunday/Exercise LeetCode/Bai 1 - Two Sum/solution-4.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j]
+      }
+    }
+  }
+  return [] // If no solution is found
+}
+
+// Example usage:
+const nums = [2, 7, 11, 15]
+const target = 9
+console.log(twoSum(nums, target)) // Output: [0, 1]
