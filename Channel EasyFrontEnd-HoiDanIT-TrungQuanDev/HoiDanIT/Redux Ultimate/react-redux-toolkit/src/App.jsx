3a2abf478bf574448dd8e2094734e52828484314
duynghiadev <duynghia22302@gmail.com>
2024-02-23 08:58:09 +0700

    #29 Fetching Data Không Sử Dụng Redux | Khóa Học Redux Ultimate

--- Channel EasyFrontEnd-HoiDanIT-TrungQuanDev/HoiDanIT/Redux Ultimate/react-redux-toolkit/src/App.jsx
+++ Channel EasyFrontEnd-HoiDanIT-TrungQuanDev/HoiDanIT/Redux Ultimate/react-redux-toolkit/src/App.jsx
@@ -1,22 +1,56 @@
 import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
 import { decrement, increment } from './redux/slices/counterSlice'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const App = () => {
   const dispatch = useDispatch()
   const count = useSelector((state) => state.counter.value)
-  console.log('count:', count)
+
+  const [listUsers, setListUsers] = useState([])
+
+  useEffect(() => {
+    fetchAllUser()
+  }, [])
+
+  const fetchAllUser = async () => {
+    const res = await axios('http://localhost:8080/users/all')
+    setListUsers(res.data ? res.data : [])
+  }
 
   return (
     <div className='App'>
       <header className='App-header'>
-        <h3>hello Redux ToolKit</h3>
+        {/* <h3>hello Redux ToolKit</h3>
         <div className='btn-actions'>
           <button onClick={() => dispatch(increment())}>Increase</button>
           <button onClick={() => dispatch(decrement())}>Decrease</button>
         </div>
         <br />
-        <div>Count: {count}</div>
+        <div>Count: {count}</div> */}
+        <div>
+          <table>
+            <thead>
+              <th>ID</th>
+              <th>Email</th>
+              <th>Username</th>
+            </thead>
+            <tbody>
+              {listUsers &&
+                listUsers.length > 0 &&
+                listUsers.map((item, index) => {
+                  return (
+                    <tr key={`table-redux-${index}`}>
+                      <td>{item.id}</td>
+                      <td>{item.email}</td>
+                      <td>{item.username}</td>
+                    </tr>
+                  )
+                })}
+            </tbody>
+          </table>
+        </div>
       </header>
     </div>
   )
