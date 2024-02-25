3b8a993b4382744cb71a1b3d479ece17fa536b6e
duynghiadev <duynghia22302@gmail.com>
2024-02-25 13:36:20 +0700

    Giỏ hàng với Redux - ReactJs

---
+++ CodeGame-ReactRouterV6-Redux-ReduxToolkit/Redux/cart_redux/src/dummy/product.js
@@ -0,0 +1,10 @@
+export const product = [
+  { id: 1, name: 'Cà phê', price: 30000 },
+  { id: 2, name: 'Cà phê sữa', price: 20000 },
+  { id: 3, name: 'Cà phê đen', price: 10000 },
+  { id: 4, name: 'Cà phê trắng', price: 40000 },
+  { id: 5, name: 'Cà phê vàng', price: 35000 },
+  { id: 6, name: 'Cà phê tím', price: 1000 },
+  { id: 7, name: 'Cà phê xanh', price: 20000 },
+  { id: 8, name: 'Cà phê đá', price: 25000 }
+]
