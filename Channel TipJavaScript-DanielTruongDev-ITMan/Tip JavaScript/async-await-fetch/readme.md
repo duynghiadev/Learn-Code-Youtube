9c9ce159111a71ace73e3086bd8d6732e19f0532
duynghiadev <duynghia22302@gmail.com>
2024-02-26 13:02:14 +0700

    Senior sử dụng async await fetch api thực sự pro nhu thế nào? | FETCH API

---
+++ Channel TipJavaScript-DanielTruongDev-ITMan/Tip JavaScript/async-await-fetch/readme.md
@@ -0,0 +1,3 @@
+# Senior sử dụng async await fetch api thực sự pro nhu thế nào? | FETCH API
+
+[Link video](https://s.net.vn/dLvE)
---
+++ Channel TipJavaScript-DanielTruongDev-ITMan/Tip JavaScript/async-await-fetch/script.js
@@ -0,0 +1,38 @@
+const initApp = async () => {
+  const url = 'https://jsonplaceholder.typicode.com/todos/1'
+
+  // Level 2
+  const getTodo = async () => {
+    return await fetch(url)
+  }
+
+  // Level 3
+  const getTodo2 = async () => {
+    return (await fetch(url)).json()
+  }
+
+  console.log('Init DOM')
+
+  const btn_getList = document.querySelector('button')
+  if (btn_getList) {
+    btn_getList.addEventListener('click', async () => {
+      console.log('Event click')
+      // Cach 1 -> Level 1
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => console.log('This is type code of Level 1 -> data:', data))
+        .catch((error) => console.log(error))
+
+      // Cach 2 -> Level 2
+      const todo = await getTodo(url)
+      const data = await todo.json()
+      console.log('This is type code of Level 2 -> data:', data)
+
+      // Cach 3 -> Level 3
+      const title = await getTodo2(url)
+      console.log('This is type code of Level 3 -> title:', title)
+    })
+  }
+}
+
+document.addEventListener('DOMContentLoaded', initApp())
