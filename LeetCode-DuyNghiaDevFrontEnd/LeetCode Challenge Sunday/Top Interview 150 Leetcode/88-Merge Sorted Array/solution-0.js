452188b6ea34841ab39e91181b5db2bbd29258aa
duynghiadev <duynghia22302@gmail.com>
2024-02-28 09:57:09 +0700

    88-Merge Sorted Array

--- LeetCode-DuyNghiaDevFrontEnd/LeetCode Challenge Sunday/Top Interview 150 Leetcode/88-Merge Sorted Array/solution-0.js
+++ LeetCode-DuyNghiaDevFrontEnd/LeetCode Challenge Sunday/Top Interview 150 Leetcode/88-Merge Sorted Array/solution-0.js
@@ -1,3 +1,11 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
 var merge = function (nums1, m, nums2, n) {
   const nums1Copy = nums1.slice(0, m) // Make a copy of the first m elements of nums1
   let i = 0,
