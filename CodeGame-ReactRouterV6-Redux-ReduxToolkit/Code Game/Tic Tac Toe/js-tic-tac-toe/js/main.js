86a6ab6161c0e9ea4c30bc674ce1dca9f984415b
duynghiadev <duynghia22302@gmail.com>
2024-03-04 06:19:03 +0700

    Update code

---
+++ CodeGame-ReactRouterV6-Redux-ReduxToolkit/Code Game/Tic Tac Toe/js-tic-tac-toe/js/main.js
@@ -0,0 +1,22 @@
+/**
+ * Global variables
+ */
+let currentTurn = "cross";
+let isGameEnded = false;
+let cellValues = new Array(9).fill("");
+
+/**
+ * TODOs
+ *
+ * 1. Bind click event for all cells
+ * 2. On cell click, do the following:
+ *    - Toggle current turn
+ *    - Mark current turn to the selected cell
+ *    - Check game state: win, ended or playing
+ *    - If game is win, highlight win cells
+ *    - Not allow to re-click the cell having value.
+ *
+ * 3. If game is win or ended --> show replay button.
+ * 4. On replay button click --> reset game to play again.
+ *
+ */
---
+++ CodeGame-ReactRouterV6-Redux-ReduxToolkit/Code Game/Tic Tac Toe/js-tic-tac-toe/js/selectors.js
@@ -0,0 +1,9 @@
+// Implement selector function to get elements needed
+// 1. Cell List
+// 2. Current Turn
+// 3. Replay Game
+// 4. Game status
+export function getCellElementList() {}
+export function getCurrentTurnElement() {}
+export function getCellElementAtIdx() {}
+export function getGameStatusElement() {}
