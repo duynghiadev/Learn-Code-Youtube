3b20f6cb54520bed090db5152bee70408687fae8
duynghiadev <duynghia22302@gmail.com>
2024-03-03 14:16:06 +0700

    Add file .prettier to format code for beautiful

--- Remain Code Again From Learn Code Youtube/Learn React Udemy/learn-reactjs-hau-nguyen/Learn JavaScript Basic/video06/exercise-02/script.js
+++ Remain Code Again From Learn Code Youtube/Learn React Udemy/learn-reactjs-hau-nguyen/Learn JavaScript Basic/video06/exercise-02/script.js
@@ -1,21 +1,18 @@
-(function () {
-  function fetchPosts(){
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response=>response.json())
-    .then((posts)=>{
-      console.log('Fetched Posts:',posts)
+(function() {
+  function fetchPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json()).then((posts) => {
+      console.log('Fetched Posts:', posts)
 
       displayPosts(posts)
-    })
-      .catch((error)=>{
-        console.error('Error fetching posts:',error)
+    }).catch((error) => {
+      console.error('Error fetching posts:', error)
     })
   }
 
-  function displayPosts(posts){
+  function displayPosts(posts) {
     const postList = document.createElement('ul')
 
-    posts.forEach((post)=>{
+    posts.forEach((post) => {
       const listItem = document.createElement('li')
       listItem.textContent = post.title
       postList.appendChild(listItem)
