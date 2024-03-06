d65dab89deea635dbbc3e0acfd91cceacf6436d0
duynghiadev <duynghia22302@gmail.com>
2024-03-06 21:53:41 +0700

    6. CSS style trong Next.js

--- channel Laptrinhlachuyennho-CodersX-DuocDev/DuocDev/Next14/client/src/app/page.tsx
+++ channel Laptrinhlachuyennho-CodersX-DuocDev/DuocDev/Next14/client/src/app/page.tsx
@@ -2,17 +2,18 @@ import Image from 'next/image'
 import Header from '@/app/Header'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import Card from '@/app/Card'
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Header />
-      <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
+      <Card />
+      {/* <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
         <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
           Xin chào các bạn, đây là khóa học NextJS.14
           <code className='font-mono font-bold'>src/app/page.tsx</code>
         </p>
-        <Button className='p-10' variant={'outline'}>
+        <Button className='p-10 text-mini' variant={'outline'}>
           Mua khóa học NextJS 14
         </Button>
         <Input type='text' name='firstName' />
@@ -116,7 +117,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   )
 }
