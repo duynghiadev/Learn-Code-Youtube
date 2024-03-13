2cd2f0870624744bc51b3b7e62f3f865095977b0
duynghiadev <duynghia22302@gmail.com>
2024-03-13 16:34:40 +0700

    Hãy từ bỏ lập trình nếu thiếu kiến thức này | Tư duy lập trình | Tự học dev (P2)

--- Channel DevXinDay-VDEV-TuHocDev/TuHocDev/GiaiThuat/tinhgiaithua.js
+++ Channel DevXinDay-VDEV-TuHocDev/TuHocDev/GiaiThuat/tinhgiaithua.js
@@ -1,6 +1,7 @@
 // tính giai thừa
 // n! = 1 x 2 x 3 x...x n
 
+// sử dụng vòng lặp for
 function tinhGiaiThua(n) {
   let ketqua = 1
   for (let i = 1; i <= n; i++) {
@@ -10,4 +11,18 @@ function tinhGiaiThua(n) {
 }
 
 let result = tinhGiaiThua(5)
-console.log(result)
+console.log('vòng lặp for:', result)
+
+console.log('===========================================')
+
+// sử dụng đệ quy
+function tinhDeQuy(n) {
+  if (n <= 1) {
+    return 1
+  }
+  console.log(`n: ${n}, n-1: ${n - 1}`)
+  return n * tinhDeQuy(n - 1)
+}
+
+let result_1 = tinhDeQuy(5)
+console.log('đệ quy:', result_1)
