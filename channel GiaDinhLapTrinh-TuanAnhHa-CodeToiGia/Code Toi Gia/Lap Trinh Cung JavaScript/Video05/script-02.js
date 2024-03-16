71240daf2683518dade71cd839030b0b176fdff7
duynghiadev <duynghia22302@gmail.com>
2024-03-16 16:09:17 +0700

    Luyện code cùng ngôn ngữ javascript

--- channel GiaDinhLapTrinh-TuanAnhHa-CodeToiGia/Code Toi Gia/Lap Trinh Cung JavaScript/Video05/script-02.js
+++ channel GiaDinhLapTrinh-TuanAnhHa-CodeToiGia/Code Toi Gia/Lap Trinh Cung JavaScript/Video05/script-02.js
@@ -12,3 +12,13 @@ const isCheck = a.length === b.length && checkArray
 
 console.log('------------------------------')
 console.log('isCheck:', isCheck)
+
+/**
+ * Explanation:
+ *
+ * - We first compare the lengths of arrays a and b to ensure they are equal
+ *
+ * - Then, we use the every() method to iterate through each element of array a and check if it matches the corresponding element in array b. If any pair of elements doesn't match, every() will return false, indicating that the arrays are not identical
+ *
+ * - If both conditions are met (equal lengths and all elements match), isCheck will be true, otherwise false.
+ */
