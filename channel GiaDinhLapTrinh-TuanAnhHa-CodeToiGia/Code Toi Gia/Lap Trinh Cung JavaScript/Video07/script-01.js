86060c47822eb50681a0e0341838cdb084489c02
duynghiadev <duynghia22302@gmail.com>
2024-03-16 17:54:11 +0700

    Luyện code cùng ngôn ngữ javascript

--- channel GiaDinhLapTrinh-TuanAnhHa-CodeToiGia/Code Toi Gia/Lap Trinh Cung JavaScript/Video07/script-01.js
+++ channel GiaDinhLapTrinh-TuanAnhHa-CodeToiGia/Code Toi Gia/Lap Trinh Cung JavaScript/Video07/script-01.js
@@ -32,3 +32,23 @@ const newUsers = users.filter((item) => {
 })
 
 console.log('newUsers:', newUsers)
+
+/**
+ * users is an array containing objects representing user data.
+ *
+ * - The filter() method is used on the users array to create a new array, newUsers, containing only the elements that satisfy the condition specified in the callback function.
+ *
+ * - In the callback function:
+ *
+ * - Object.values(item) returns an array containing all the values of the current item object.
+ *
+ * - some((i) => !i) checks if there is at least one falsy value (!i) in the array of values. If any value is falsy (empty string, zero, etc.), some() returns true; otherwise, it returns false.
+ *
+ * - !Object.values(item).some((i) => !i) negates the result of some(). If there is no falsy value in the array of values (i.e., all values are truthy), !some() evaluates to true; otherwise, it evaluates to false.
+ *
+ * - So, the filter() method creates newUsers array containing only those user objects for which !Object.values(item).some((i) => !i) evaluates to true. In other words, it filters out the objects where at least one property has a falsy value.
+ *
+ * - Finally, newUsers array is logged to the console.
+ *
+ * - This code effectively filters out user objects that have at least one property with a falsy value, leaving only those with all properties having truthy values.
+ */
--- channel GiaDinhLapTrinh-TuanAnhHa-CodeToiGia/Code Toi Gia/Lap Trinh Cung JavaScript/Video07/script-03.js
+++ channel GiaDinhLapTrinh-TuanAnhHa-CodeToiGia/Code Toi Gia/Lap Trinh Cung JavaScript/Video07/script-03.js
@@ -21,3 +21,15 @@ const newUsers = users.filter((user) => {
 })
 
 console.log('newUsers:', newUsers)
+
+/**
+ * Explanation:
+ *
+ * - We use the filter() method to iterate over each user object in the users array
+ *
+ * - For each user object, we use Object.values() to get an array of its property values
+ *
+ * - Then, we use the every() method to check if all values are truthy. If any value is falsy (e.g., empty string, zero, etc.), the every() method returns false, causing the user object to be excluded from the newUsers array
+ *
+ * - Finally, we log the newUsers array containing only the user objects with all properties having truthy values.
+ */
