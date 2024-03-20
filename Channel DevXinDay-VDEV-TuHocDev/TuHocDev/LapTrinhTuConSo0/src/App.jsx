aae9242e1b901bbe742ce0177bef3a3be55cb8d0
duynghiadev <duynghia22302@gmail.com>
2024-03-21 06:39:41 +0700

    Phần 2: Hướng dẫn lập trình từ con số 0

--- Channel DevXinDay-VDEV-TuHocDev/TuHocDev/LapTrinhTuConSo0/src/App.jsx
+++ Channel DevXinDay-VDEV-TuHocDev/TuHocDev/LapTrinhTuConSo0/src/App.jsx
@@ -1,34 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <h2>Hello world</h2>
+    </div>
   )
 }
 
