12fe57921ede9184bbcc2468d398ae355c55a2a7
duynghiadev <duynghia22302@gmail.com>
2024-03-21 06:50:04 +0700

    Phần 3: Hướng dẫn lập trình từ con số 0

--- Channel DevXinDay-VDEV-TuHocDev/TuHocDev/LapTrinhTuConSo0/src/App.jsx
+++ Channel DevXinDay-VDEV-TuHocDev/TuHocDev/LapTrinhTuConSo0/src/App.jsx
@@ -1,11 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './App.css'
+import Header from './components/header/header'
 
 function App() {
   return (
-    <div className='App'>
-      <h2>Hello world</h2>
+    <div>
+      <Header />
+      <div className='row'>
+        <div className='col-lg-7 leftlayout'>left</div>
+        <div className='col-lg-5 rightlayout'>right</div>
+      </div>
     </div>
   )
 }
---
+++ Channel DevXinDay-VDEV-TuHocDev/TuHocDev/LapTrinhTuConSo0/src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h2>header</h2>
+    </div>
+  )
+}
+
+export default Header
