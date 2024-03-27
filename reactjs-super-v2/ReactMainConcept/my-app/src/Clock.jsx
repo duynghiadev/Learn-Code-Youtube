92b025f65f7af649f2200929767b5862864f24c8
duynghiadev <duynghia22302@gmail.com>
2024-03-27 16:13:56 +0700

    Bài 78: componentdidMount

--- reactjs-super-v2/ReactMainConcept/my-app/src/Clock.jsx
+++ reactjs-super-v2/ReactMainConcept/my-app/src/Clock.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 
+const lists = ['BMW', 'Toyota', 'Honda']
+
+const fetchApi = () => {
+  return new Promise((reject, resolve) => {
+    setTimeout(() => {
+      resolve(lists)
+    }, 1000)
+  })
+}
+
 export default class Clock extends React.Component {
   constructor(props) {
     super(props)
@@ -10,10 +20,23 @@ export default class Clock extends React.Component {
       seconds: {
         created: new Date().getSeconds()
       },
-      name: this.props.name
+      name: this.props.name,
+      lists: []
     }
     this.date = '22/8/2022'
     this.getTime = this.getTime.bind(this)
+  }
+
+  componentDidMount() {
+    const seconds = document.getElementById('seconds')
+    console.log('seconds:', seconds)
+
+    fetchApi().then((res) =>
+      this.setState((prevState) => ({
+        ...prevState,
+        lists: res
+      }))
+    )
   }
 
   getTime() {
@@ -31,12 +54,14 @@ export default class Clock extends React.Component {
   }
 
   render() {
-    console.log(this.props)
+    console.log('this.props:', this.props)
+    console.log('this.state:', this.state)
+
     return (
       <div>
         <h1>Hello, world! {this.state.name}</h1>
         <h2>It is {this.state.time.created}</h2>
-        <h2>It is {this.state.seconds.created}</h2>
+        <h2 id='seconds'>It is {this.state.seconds.created}</h2>
 
         <h3>Is is {this.date}</h3>
         <button onClick={this.getTime}>Get Time</button>
