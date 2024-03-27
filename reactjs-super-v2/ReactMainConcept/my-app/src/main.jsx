087bf62c360132b0c484434d8ab48bf3d35351f2
duynghiadev <duynghia22302@gmail.com>
2024-03-27 13:42:07 +0700

    Bài 74: Render element

--- reactjs-super-v2/ReactMainConcept/my-app/src/main.jsx
+++ reactjs-super-v2/ReactMainConcept/my-app/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+// const element = <h1>Hello world</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello world</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  )
+  root.render(element)
+}
+
+setInterval(tick, 1000)
