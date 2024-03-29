1b4e2882986a373be2384d60a3cbe7a3b0c980f9
duynghiadev <duynghia22302@gmail.com>
2024-03-29 16:55:49 +0700

    Bài 81: Cách dùng Props và PropTypes

--- reactjs-super-v2/ReactMainConcept/my-app/src/App.jsx
+++ reactjs-super-v2/ReactMainConcept/my-app/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
+import BareInput from './BareInput'
 import Clock from './Clock'
+import Layout from './Layout'
 
 function App() {
   const [name, setName] = useState('Casio')
@@ -11,6 +13,17 @@ function App() {
       <button onClick={() => setName('Apple')}>Change name</button>
       <button onClick={() => setVisible(false)}>Hide Clock component</button>
       {visible && <Clock name={name} />}
+
+      <Layout>
+        <h1>Hello</h1>
+        <BareInput
+          type='text'
+          value='100'
+          autoFocus
+          className='input-control'
+          onChange={() => {}}
+        />
+      </Layout>
     </div>
   )
 }
--- reactjs-super-v2/ReactMainConcept/my-app/src/Welcome.jsx
+++ reactjs-super-v2/ReactMainConcept/my-app/src/Welcome.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
 
-// const Welcome = (props) => {
-//   const { name, age } = props
+// functional component
+const Welcome = (props) => {
+  const { name, age } = props
 
-//   return (
-//     <div>
-//       <h1>Hello, {name} - {age}</h1>
-//     </div>
-//   )
-// }
-
-class Welcome extends React.Component {
-  render() {
-    console.log(this.props)
-    return (
-      <>
-        <h1>
-          Hello, {this.props.name} - {this.props.age}
-        </h1>
-      </>
-    )
-  }
+  return (
+    <div>
+      <h1>
+        Hello, {name} - {age}
+      </h1>
+    </div>
+  )
 }
+
+// class component
+// class Welcome extends React.Component {
+//   render() {
+//     console.log(this.props)
+//     return (
+//       <>
+//         <h1>
+//           Hello, {this.props.name} - {this.props.age}
+//         </h1>
+//       </>
+//     )
+//   }
+// }
 
 export default Welcome
