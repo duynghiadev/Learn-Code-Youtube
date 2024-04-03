a61b62b7be803dbe2f645f70a4747a89d6e587e7
duynghiadev <duynghia22302@gmail.com>
2024-04-03 17:14:06 +0700

    Giao diện 1: User Card

--- channel TuTran-PheCode-PhamHuyHoang/PhamHuyHoang/Code UI React and TailwindCSS/src/App.tsx
+++ channel TuTran-PheCode-PhamHuyHoang/PhamHuyHoang/Code UI React and TailwindCSS/src/App.tsx
@@ -3,6 +3,7 @@ import { UserProfile } from './One/UserProfile'
 function App() {
   return (
     <h1 className='flex h-screen w-full items-center justify-center bg-[#E6EFFA]'>
+      {/* Giao diện 1: User Card */}
       <UserProfile />
     </h1>
   )
