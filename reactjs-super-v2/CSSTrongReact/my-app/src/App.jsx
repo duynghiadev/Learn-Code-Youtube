f6d0241322f27d160689bb7c39cac18f4b21f0d8
duynghiadev <duynghia22302@gmail.com>
2024-04-03 18:29:27 +0700

    Bài 93: Vấn đề Global Scoped CSS trong React

---
+++ reactjs-super-v2/CSSTrongReact/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Cart from './Cart'
+import ProductList from './ProductList'
+
+function App() {
+  return (
+    <div>
+      <Cart />
+      <ProductList />
+    </div>
+  )
+}
+
+export default App
---
+++ reactjs-super-v2/CSSTrongReact/my-app/src/ProductList.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import './productList.css'
+
+export default function ProductList() {
+  return <div className='container'>ProductList</div>
+}
