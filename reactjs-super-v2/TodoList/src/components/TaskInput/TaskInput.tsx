f771c43da7e62ef6fd37422fc5d2ae548f0e5312
duynghiadev <duynghia22302@gmail.com>
2024-04-08 22:29:12 +0700

    Bài 110: Code chức năng thêm và hoàn thành

--- reactjs-super-v2/TodoList/src/components/TaskInput/TaskInput.tsx
+++ reactjs-super-v2/TodoList/src/components/TaskInput/TaskInput.tsx
@@ -1,11 +1,30 @@
+import { useState } from 'react'
 import styles from './taskInput.module.scss'
 
-export default function TaskInput() {
+interface TaskInputProps {
+  addTodo: (name: string) => void
+}
+
+export default function TaskInput(props: TaskInputProps) {
+  const { addTodo } = props
+  const [name, setName] = useState<string>('')
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    addTodo(name)
+    setName('')
+  }
+
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target
+    setName(value)
+  }
+
   return (
     <div className='mb-2'>
       <h1 className={styles.title}>To do list typescript</h1>
-      <form className={styles.form}>
-        <input type='text' placeholder='caption goes here' />
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input type='text' placeholder='caption goes here' value={name} onChange={onChangeInput} />
         <button type='submit'>➕</button>
       </form>
     </div>
--- reactjs-super-v2/TodoList/src/components/TodoList/TodoList.tsx
+++ reactjs-super-v2/TodoList/src/components/TodoList/TodoList.tsx
@@ -1,15 +1,42 @@
+import { useState } from 'react'
 import TaskInput from '../TaskInput'
 import TaskList from '../TaskList'
+import { Todo } from '../../@types/todo.type'
 import styles from './todoList.module.scss'
 
 export default function TodoList() {
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const doneTodos = todos.filter((todo) => todo.done)
+  const notdoneTodos = todos.filter((todo) => !todo.done)
+
+  const addTodo = (name: string) => {
+    const todo: Todo = {
+      name,
+      done: false,
+      id: new Date().toISOString()
+    }
+    setTodos((prev) => [...prev, todo])
+  }
+
+  const handleDoneTodo = (id: string, done: boolean) => {
+    setTodos((prev) => {
+      return prev.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, done }
+        }
+        return todo
+      })
+    })
+  }
+
+  console.log(todos)
   return (
     <div className={styles.todoList}>
       <div className={styles.todoListContainer}>
-        TodoList
-        <TaskInput />
-        <TaskList doneTaskList={true} />
-        <TaskList doneTaskList={false} />
+        <TaskInput addTodo={addTodo} />
+        <TaskList todos={notdoneTodos} handleDoneTodo={handleDoneTodo} />
+        <TaskList doneTaskList todos={doneTodos} handleDoneTodo={handleDoneTodo} />
       </div>
     </div>
   )
