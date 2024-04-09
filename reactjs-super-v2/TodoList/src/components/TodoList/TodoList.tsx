90871ba0cd417892c631afcf4e15545f96ed0aab
duynghiadev <duynghia22302@gmail.com>
2024-04-09 09:58:05 +0700

    Bài 111: Code chức năng edit

--- reactjs-super-v2/TodoList/src/components/TodoList/TodoList.tsx
+++ reactjs-super-v2/TodoList/src/components/TodoList/TodoList.tsx
@@ -6,6 +6,7 @@ import styles from './todoList.module.scss'
 
 export default function TodoList() {
   const [todos, setTodos] = useState<Todo[]>([])
+  const [currentTodo, setCurrentTodo] = useState<Todo | null>(null)
 
   const doneTodos = todos.filter((todo) => todo.done)
   const notdoneTodos = todos.filter((todo) => !todo.done)
@@ -30,13 +31,42 @@ export default function TodoList() {
     })
   }
 
-  console.log(todos)
+  const startEditTodo = (id: string) => {
+    const findedTodo = todos.find((todo) => todo.id === id)
+    if (findedTodo) {
+      setCurrentTodo(findedTodo)
+    }
+  }
+
+  const editTodo = (name: string) => {
+    setCurrentTodo((prev) => {
+      if (prev) {
+        return { ...prev, name }
+      }
+      return null
+    })
+  }
+
+  const finishEditTodo = () => {
+    setTodos((prev) => {
+      console.log('🔄 Updating todos in setTodos of finishEditTodo:', prev)
+      return prev.map((todo) => {
+        console.log('➡️ Processing todo in prev.map() of finishEditTodo:', todo)
+        if (todo.id === (currentTodo as Todo).id) {
+          return currentTodo as Todo
+        }
+        return todo
+      })
+    })
+    setCurrentTodo(null)
+  }
+
   return (
     <div className={styles.todoList}>
       <div className={styles.todoListContainer}>
-        <TaskInput addTodo={addTodo} />
-        <TaskList todos={notdoneTodos} handleDoneTodo={handleDoneTodo} />
-        <TaskList doneTaskList todos={doneTodos} handleDoneTodo={handleDoneTodo} />
+        <TaskInput addTodo={addTodo} currentTodo={currentTodo} editTodo={editTodo} finishEditTodo={finishEditTodo} />
+        <TaskList todos={notdoneTodos} handleDoneTodo={handleDoneTodo} startEditTodo={startEditTodo} />
+        <TaskList doneTaskList todos={doneTodos} handleDoneTodo={handleDoneTodo} startEditTodo={startEditTodo} />
       </div>
     </div>
   )
