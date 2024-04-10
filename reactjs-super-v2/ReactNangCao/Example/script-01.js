22885053987de5cf51b47149a68e6c7bac324db8
duynghiadev <duynghia22302@gmail.com>
2024-04-10 18:31:10 +0700

    Bài 115: Higher Order Component

---
+++ reactjs-super-v2/ReactNangCao/Example/script-01.js
@@ -0,0 +1,16 @@
+const findNumber = (num) => (func) => {
+  const result = []
+  for (let i = 0; i < num; i++) {
+    if (func(i)) {
+      result.push(i)
+    }
+  }
+  return result
+}
+const result1 = findNumber(10)((number) => number % 2 === 1) // <10 và số lẻ
+const result2 = findNumber(20)((number) => number % 2 === 0) // <20 và số chẳn
+const result3 = findNumber(30)((number) => number % 3 === 2) // <30 và số chia 3 dư 2
+
+console.log('result1', result1)
+console.log('result2', result2)
+console.log('result3', result3)
---
+++ reactjs-super-v2/ReactNangCao/TodoList/.eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'react-app',
+    'prettier'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'react', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': [
+      'warn',
+      {
+        arrowParens: 'always',
+        semi: false,
+        trailingComma: 'none',
+        tabWidth: 2,
+        endOfLine: 'auto',
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 120,
+        jsxSingleQuote: true
+      }
+    ]
+  }
+}
