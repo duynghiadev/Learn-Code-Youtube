6f20af491999ffa01bb6f59face04e2000af9740
duynghiadev <duynghia22302@gmail.com>
2024-04-11 14:36:50 +0700

    Add some example about Currying in JavaScript

--- reactjs-super-v2/ReactNangCao/Example Currying in JavaScript/script-01.js
+++ reactjs-super-v2/ReactNangCao/Example Currying in JavaScript/script-01.js
@@ -7,10 +7,11 @@ const findNumber = (num) => (func) => {
   }
   return result
 }
+
 const result1 = findNumber(10)((number) => number % 2 === 1) // <10 và số lẻ
 const result2 = findNumber(20)((number) => number % 2 === 0) // <20 và số chẳn
 const result3 = findNumber(30)((number) => number % 3 === 2) // <30 và số chia 3 dư 2
 
-console.log('result1', result1)
-console.log('result2', result2)
-console.log('result3', result3)
+console.log('result1:', result1)
+console.log('result2:', result2)
+console.log('result3:', result3)
