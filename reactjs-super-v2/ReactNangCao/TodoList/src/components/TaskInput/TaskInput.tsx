4e96a8842a02c1243b610e89afce4be849a325bf
duynghiadev <duynghia22302@gmail.com>
2024-04-12 19:07:25 +0700

    Bài 117: React.memo

--- reactjs-super-v2/ReactNangCao/TodoList/src/components/TaskInput/TaskInput.tsx
+++ reactjs-super-v2/ReactNangCao/TodoList/src/components/TaskInput/TaskInput.tsx
@@ -5,6 +5,7 @@ import styles from './taskInput.module.scss'
 import { TodoTypes } from '../../PropTypes/todo.proptype'
 import connect from '../../HOC/connect'
 import { debug, log } from '../../constants'
+import Title from '../Title'
 
 interface TaskInputProps {
   addTodo: (name: string) => void
@@ -17,6 +18,10 @@ function TaskInput(props: TaskInputProps & typeof injectedProps) {
   const { addTodo, currentTodo, editTodo, finishEditTodo, debug, log } = props
   const [name, setName] = useState<string>('')
   log(debug)
+
+  const address = {
+    street: '10 Tran Hung Dao'
+  }
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -42,7 +47,7 @@ function TaskInput(props: TaskInputProps & typeof injectedProps) {
 
   return (
     <div className='mb-2'>
-      <h1 className={styles.title}>To do list typescript</h1>
+      <Title address={address} />
       <form className={styles.form} onSubmit={handleSubmit}>
         <input
           type='text'
---
+++ reactjs-super-v2/ReactNangCao/TodoList/src/components/Title/Title.tsx
@@ -0,0 +1,19 @@
+import styles from './title.module.scss'
+import React from 'react'
+
+type TitleProps = {
+  address: {
+    street: string
+  }
+}
+
+const Title = (props: TitleProps) => {
+  console.log('prop in component Title', props)
+  return <h1 className={styles.title}>To do list typescript</h1>
+}
+
+const equal = (prevProp: TitleProps, nextProps: any) => {
+  return prevProp.address.street === nextProps.address.street
+}
+
+export default React.memo(Title, equal)
