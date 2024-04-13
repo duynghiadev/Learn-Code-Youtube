53be22aa2fbd5816f41cec880bba4ecf8429da3d
duynghiadev <duynghia22302@gmail.com>
2024-04-13 17:44:53 +0700

    move project to WebStorm Editor

--- Channel DevXinDay-VDEV-TuHocDev/TuHocDev/LapTrinhTuConSo0/src/App.jsx
+++ Channel DevXinDay-VDEV-TuHocDev/TuHocDev/LapTrinhTuConSo0/src/App.jsx
@@ -11,18 +11,18 @@ function App() {
   return (
     <div>
       <Header />
-      <div className='row my-container'>
-        <div className='col-lg-7 leftlayout'>
+      <div className="row my-container">
+        <div className="col-lg-7 leftlayout">
           <p>{tieudeungdung}</p>
           <Outlet></Outlet>
         </div>
-        <div className='col-lg-5 rightlayout'>
+        <div className="col-lg-5 rightlayout">
           <p>CÁC TÍNH NĂNG</p>
-          <div className='rightlayout-item'>
-            <NavLink to='canbachai'>Căn bậc 2</NavLink>
+          <div className="rightlayout-item">
+            <NavLink to="canbachai">Căn bậc 2</NavLink>
           </div>
-          <div className='rightlayout-item'>
-            <NavLink to='maytinhcong'>Máy tính cộng</NavLink>
+          <div className="rightlayout-item">
+            <NavLink to="maytinhcong">Máy tính cộng</NavLink>
           </div>
         </div>
       </div>
