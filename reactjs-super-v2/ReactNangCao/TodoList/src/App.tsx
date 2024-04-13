6310868602de211f99f2b6a77af51f11e39c828b
duynghiadev <duynghia22302@gmail.com>
2024-04-13 17:43:11 +0700

    Bài 121: useLayoutEffect và tiến trình paint trên trình duyệt

--- reactjs-super-v2/ReactNangCao/TodoList/src/App.tsx
+++ reactjs-super-v2/ReactNangCao/TodoList/src/App.tsx
@@ -1,13 +1,15 @@
 import TodoList from './components/TodoList'
 import Watch from './components/Watch'
 import Slider from './components/Slider/Slider'
+import Count from './components/Count'
 
 function App() {
   return (
     <div>
       {/*<TodoList />*/}
       {/*<Watch />*/}
-      <Slider />
+      {/*<Slider />*/}
+      <Count />
     </div>
   )
 }
