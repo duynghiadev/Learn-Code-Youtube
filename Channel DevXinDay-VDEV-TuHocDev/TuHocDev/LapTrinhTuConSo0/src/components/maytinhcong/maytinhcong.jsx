000a32ba60d8a326a15cfb40edf8f1b43a71ede0
duynghiadev <duynghia22302@gmail.com>
2024-04-17 22:03:31 +0700

    Phần 8: Mở rộng tính năng trong | React

--- Channel DevXinDay-VDEV-TuHocDev/TuHocDev/LapTrinhTuConSo0/src/components/maytinhcong/maytinhcong.jsx
+++ Channel DevXinDay-VDEV-TuHocDev/TuHocDev/LapTrinhTuConSo0/src/components/maytinhcong/maytinhcong.jsx
@@ -1,16 +1,33 @@
 import { useDispatch } from 'react-redux'
 import './maytinhcong.css'
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { setTieude } from '../../redux/redux.root'
 
 const Maytinhcong = () => {
+  const firstNumber = useRef()
+  const secondNumber = useRef()
+
+  const [outputValue, setOutputValue] = useState(0)
   const dispatch = useDispatch()
+
+  const tinhCong = () => {
+    const first = firstNumber.current.value
+    const second = secondNumber.current.value
+    setOutputValue(Number(first) + Number(second))
+  }
 
   useEffect(() => {
     dispatch(setTieude('Máy tính cộng'))
   }, [])
 
-  return <div></div>
+  return (
+    <div>
+      <input ref={firstNumber} />
+      <input ref={secondNumber} />
+      <button onClick={tinhCong}>Tính toán</button>
+      <p>{outputValue}</p>
+    </div>
+  )
 }
 
 export default Maytinhcong
