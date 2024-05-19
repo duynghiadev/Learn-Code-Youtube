6025aed757c88fa5039cf85a27a0bef9cbf9355b
duynghiadev <duynghia22302@gmail.com>
2024-05-19 13:24:07 +0700

    Update code in file prototype promise.js

--- channel Laptrinhlachuyennho-CodersX-DuocDev-WebStation/WebStation/JavaScript chuyên sâu/Video05-Video08/Video08/prototype promise.js
+++ channel Laptrinhlachuyennho-CodersX-DuocDev-WebStation/WebStation/JavaScript chuyên sâu/Video05-Video08/Video08/prototype promise.js
@@ -1,20 +1,21 @@
 function jsPromise(callback) {}
 
+// Define the 'then' method on jsPromise's prototype
 jsPromise.prototype.then = function () {
   console.log('method prototype then update p1 and p2')
 }
 
 const p1 = new jsPromise(function () {})
 
-p1.then()
+p1.then() // method prototype then update p1 and p2
 
 const p2 = new jsPromise(function () {})
 
-p2.then()
+p2.then() // method prototype then update p1 and p2
 
-console.log('='.repeat(40))
+console.log('='.repeat(40)) // ========================================
 
-jsPromise.prototype = 'thuộc tính'
+jsPromise.prototype = 'thuộc tính' // Replace the prototype of jsPromise with a string, removing the 'then' method
 
 jsPromise.resolve = function (value) {
   console.log('static method resolve: ' + value)
@@ -24,16 +25,15 @@ jsPromise.all = function (array) {
   console.log('static method all: ' + array)
 }
 
-jsPromise.resolve('duynghiadev')
+jsPromise.resolve('duynghiadev') // static method resolve: duynghiadev
 
-console.log('='.repeat(40))
+console.log('='.repeat(40)) // ========================================
 
 Promise.resolve(1000).then(function (data) {
-  console.log('data của Promise static: ' + data)
+  console.log('data của Promise static: ' + data) // data của Promise static: 1000
 })
 
-// Nếu như chúng ta viết như này là sẽ bị lỗi ngay. Bởi vì prototype của Promise nó không cho phép sử dụng bên ngoài
-
+// If we write like this, it will cause an error immediately. Because the prototype of Promise does not allow it to be used externally
 const promise = new Promise(function (resolve, reject) {})
 
 promise.resolve(2000).then(function (data) {
