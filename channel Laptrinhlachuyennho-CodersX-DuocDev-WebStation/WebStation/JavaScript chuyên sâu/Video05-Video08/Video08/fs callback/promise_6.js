ec6ca20ed04af6e327828b687bb96cf36cee4d98
duynghiadev <duynghia22302@gmail.com>
2024-05-19 16:17:45 +0700

    Edit code

--- channel Laptrinhlachuyennho-CodersX-DuocDev-WebStation/WebStation/JavaScript chuyên sâu/Video05-Video08/Video08/fs callback/promise_6.js
+++ channel Laptrinhlachuyennho-CodersX-DuocDev-WebStation/WebStation/JavaScript chuyên sâu/Video05-Video08/Video08/fs callback/promise_6.js
@@ -13,16 +13,19 @@ let result = []
 readPromise('text1.txt')
   .then((resolveData1) => {
     const mot = resolveData1.toString()
+    console.log('mot:', mot)
     result.push(mot)
     return readPromise('text2.txt')
   })
   .then((resolveData2) => {
     const hai = resolveData2.toString()
+    console.log('hai:', hai)
     result.push(hai)
     return readPromise('text3.txt')
   })
   .then((resolveData3) => {
     const ba = resolveData3.toString()
+    console.log('ba:', ba)
     result.push(ba)
     console.log('result:', result)
   })
