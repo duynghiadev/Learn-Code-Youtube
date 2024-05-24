57e25a1a23664221b822edff9029da8b8c7a99a1
duynghiadev <duynghia22302@gmail.com>
2024-05-24 18:01:26 +0700

    học tới phút 2:48 ⏲

---
+++ reactjs-super-v2/Redux/remote-blog/src/pages/blog/actions/blog.action.ts
@@ -0,0 +1,15 @@
+import { createAction, nanoid } from '@reduxjs/toolkit'
+import { Post } from '../../../types/blog.type'
+
+// export const addPost = createAction('blog/addPost', function (post: Omit<Post, 'id'>) {
+//   return {
+//     payload: {
+//       ...post,
+//       id: nanoid()
+//     }
+//   }
+// })
+// export const deletePost = createAction<string>('blog/deletePost')
+// export const startEditingPost = createAction<string>('blog/startEditingPost')
+// export const cancelEditingPost = createAction('blog/cancelEditingPost')
+// export const finishEditingPost = createAction<Post>('blog/finishEditingPost')
