ded92adabc8cf9daa5611c59057e324646e10080
duynghiadev <duynghia22302@gmail.com>
2024-05-31 18:51:57 +0700

    #typescript p.3 Các kiểu tiện ích trong Typescript | Utility types

--- test code javascript/viiiprock/typescript/video03/typescript09.ts
+++ test code javascript/viiiprock/typescript/video03/typescript09.ts
@@ -1,98 +1,84 @@
-// Define the Todo interface
-interface Todo {
-  id: number;
-  title: string;
-  description?: string;
-  isCompleted: boolean;
-}
-
-const todo: Todo = {
-  id: 1,
-  title: "Complete assignment",
-  description: "Finish the task by tomorrow",
-  isCompleted: false,
-};
-
 // 7:54 NonNullable
-const nonNullableTodo: NonNullable<Todo> = {
-  id: 2,
-  title: "Attend meeting",
-  description: "Meeting with team at 10:00 AM",
-  isCompleted: true,
-};
-console.log("Non-Nullable Todo:", nonNullableTodo);
-console.log("--------------------------------------------------");
+type Example = string | number | null | undefined;
+type NonNullableExample = NonNullable<Example>; // string | number
+const exampleValue: Example = "hello";
+const nonNullableValue: NonNullableExample = exampleValue;
+console.log("NonNullable:", nonNullableValue); // Output: "hello"
 
 // 9:40 Parameters
-function logTodoDetails(todo: Todo) {
-  console.log("Logged Todo:", todo);
-}
-
-logTodoDetails(todo);
-console.log("--------------------------------------------------");
+function exampleFunction1(a: string, b: number): void {}
+type Params = Parameters<typeof exampleFunction1>; // [string, number]
+const params: Params = ["hello", 42];
+console.log("Parameters:", params); // Output: ["hello", 42]
 
 // 11:40 ConstructorParameters
-class TodoList {
-  constructor(...todos: Todo[]) {
-    console.log("Todos:", todos);
-  }
+class ExampleClass {
+  constructor(a: string, b: number) {}
 }
-
-new TodoList(todo, nonNullableTodo);
-console.log("--------------------------------------------------");
+type ConstructorParams = ConstructorParameters<typeof ExampleClass>; // [string, number]
+const constructorParams: ConstructorParams = ["hello", 42];
+console.log("ConstructorParameters:", constructorParams); // Output: ["hello", 42]
 
 // 12:48 ReturnType
-function createTodo(): Todo {
-  return {
-    id: 3,
-    title: "Buy groceries",
-    isCompleted: false,
-  };
+function exampleFunction2(a: string, b: number): boolean {
+  return true;
 }
-
-const createdTodo = createTodo();
-console.log("Created Todo:", createdTodo);
-console.log("--------------------------------------------------");
+type ReturnTypeExample = ReturnType<typeof exampleFunction2>; // boolean
+const returnValue: ReturnTypeExample = exampleFunction2("hello", 42);
+console.log("ReturnType:", returnValue); // Output: true
 
 // 13:37 InstanceType
-class TodoManager {
-  todo: Todo;
-  constructor(todo: Todo) {
-    this.todo = todo;
-  }
+class ExampleClass2 {
+  a: string;
+  b: number;
 
-  displayTodo() {
-    console.log("Todo:", this.todo);
+  constructor() {
+    this.a = "";
+    this.b = 0;
   }
 }
-
-const todoInstance = new TodoManager(todo);
-todoInstance.displayTodo();
-console.log("--------------------------------------------------");
+type InstanceTypeExample = InstanceType<typeof ExampleClass2>; // ExampleClass2
+const instanceValue: InstanceTypeExample = new ExampleClass2();
+instanceValue.a = "hello";
+instanceValue.b = 42;
+console.log("InstanceType:", instanceValue); // Output: ExampleClass2 { a: "hello", b: 42 }
 
 // 14:17 ThisParameterType
-function logTodoTitle(this: Todo) {
-  console.log("Todo Title:", this.title);
+function exampleFunction3(this: Date, a: string): void {
+  console.log("ThisParameterType: this date is", this.toDateString());
 }
-
-const todoThisParameterType = logTodoTitle.bind(todo);
-todoThisParameterType();
-console.log("--------------------------------------------------");
+type ThisParamType = ThisParameterType<typeof exampleFunction3>; // Date
+const thisParamValue: ThisParamType = new Date();
+exampleFunction3.call(thisParamValue, "hello"); // Output: ThisParameterType: this date is ...
 
 // 15:25 OmitThisParameter
-function logMessage() {
-  console.log("A generic log message");
+function exampleFunction4(this: Date, a: string): void {
+  console.log("OmitThisParameter: a is", a);
 }
-
-const todoWithoutThisParameter = logMessage;
-todoWithoutThisParameter();
-console.log("--------------------------------------------------");
+type OmitThisParamType = OmitThisParameter<typeof exampleFunction4>; // (a: string) => void
+const omitThisParamFunction: OmitThisParamType = exampleFunction4.bind(
+  new Date()
+);
+omitThisParamFunction("hello"); // Output: OmitThisParameter: a is hello
 
 // 16:05 ThisType
-const todoThisType: ThisType<Todo> = {
-  id: 4,
-  title: "Walk the dog",
-  isCompleted: false,
+type ObjectDescriptor<D, M> = {
+  data?: D;
+  methods?: M & ThisType<D & M>; // this sẽ có kiểu D & M
 };
-console.log("This Type Todo:", todoThisType);
-console.log("--------------------------------------------------");
+
+let obj: ObjectDescriptor<{ x: number }, { getX(): number }> = {
+  data: { x: 10 },
+  methods: {
+    getX() {
+      return this.x;
+    },
+  },
+};
+
+if (obj.data && obj.methods) {
+  const instance = { ...obj.data, ...obj.methods };
+  console.log("ThisType:", instance.getX()); // Output: ThisType: 10
+} else {
+  console.log("ThisType: data or methods is undefined");
+}
