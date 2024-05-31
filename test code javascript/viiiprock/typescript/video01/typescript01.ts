ea361cc3a62d216a0d5194eef17edb2c6dfa7bbd
duynghiadev <duynghia22302@gmail.com>
2024-05-31 12:32:27 +0700

    Add folder typescript

--- test code javascript/viiiprock/typescript/video01/typescript01.ts
+++ test code javascript/viiiprock/typescript/video01/typescript01.ts
@@ -1,21 +1,21 @@
 function greeting(name: string) {
-  console.log('Hello: ' + name)
+  console.log("Hello: " + name);
 }
 
-greeting('Duynghiadev')
+greeting("Duynghiadev");
 
 function foo({ first, last }: { first: string; last?: string }) {
-  console.log(first + ' ' + (last || ''))
+  console.log(first + " " + (last || ""));
 }
 
-foo({ first: 'John' })
+foo({ first: "John" });
 
 function printId(id: number | string) {
-  if (typeof id === 'string') {
-    console.log('Your id: ', id.toUpperCase())
+  if (typeof id === "string") {
+    console.log("Your id: ", id.toUpperCase());
   } else {
-    console.log(id)
+    console.log(id);
   }
 }
 
-printId(123123)
+printId(123123);
