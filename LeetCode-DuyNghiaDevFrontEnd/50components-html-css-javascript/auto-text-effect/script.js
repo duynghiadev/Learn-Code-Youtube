4b9cdf963b76d7ccc3b7a9bfc04bc2d36dc1cd59
duynghiadev <duynghia22302@gmail.com>
2024-06-03 23:24:36 +0700

    format code

--- LeetCode-DuyNghiaDevFrontEnd/50components-html-css-javascript/auto-text-effect/script.js
+++ LeetCode-DuyNghiaDevFrontEnd/50components-html-css-javascript/auto-text-effect/script.js
@@ -7,16 +7,12 @@ let speed = 300 / speedEl.value
 writeText()
 
 function writeText() {
-    textEl.innerText = text.slice(0, idx)
-
-    idx++
-
-    if(idx > text.length) {
-        idx = 1
-    }
-
-    setTimeout(writeText, speed)
+  textEl.innerText = text.slice(0, idx)
+  idx++
+  if (idx > text.length) {
+    idx = 1
+  }
+  setTimeout(writeText, speed)
 }
-
 
 speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
