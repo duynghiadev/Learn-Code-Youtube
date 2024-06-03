5123b5c661feb24e86c6a90b71136279913b3748
duynghiadev <duynghia22302@gmail.com>
2024-06-03 16:40:38 +0700

    02:09:00 [6.2/6] Xây dựng Todo App bằng REDUX Toolkit

--- Channel F8-CoderX-HoleTex-NineDev/HoleTex/Redux and Redux Thunk/todo-app-redux/src/react-redux-toolkit/components/Filters/FiltersSlice.js
+++ Channel F8-CoderX-HoleTex-NineDev/HoleTex/Redux and Redux Thunk/todo-app-redux/src/react-redux-toolkit/components/Filters/FiltersSlice.js
@@ -1,31 +1,22 @@
-const initState = {
-  search: "",
-  status: "All",
-  priorities: [],
-};
+import { createSlice } from "@reduxjs/toolkit";
 
-const filtersReducer = (state = initState, action) => {
-  switch (action.type) {
-    case "filters/searchFilterChange":
-      return {
-        ...state,
-        search: action.payload,
-      };
-
-    case "filters/statusFilterChange":
-      return {
-        ...state,
-        status: action.payload,
-      };
-
-    case "filters/prioritiesFilterChange":
-      return {
-        ...state,
-        priorities: action.payload,
-      };
-    default:
-      return state;
-  }
-};
-
-export default filtersReducer;
+export const filtersSlice = createSlice({
+  name: "filters",
+  initialState: {
+    search: "",
+    status: "All",
+    priorities: [],
+  },
+  reducers: {
+    searchFilterChange: (state, action) => {
+      // mutation -> trong thư viện redux toolkit nó cài đặt sẵn cho chúng ta thư viện immer
+      state.search = action.payload;
+    }, // { type: 'filters/searchFilterChange' }
+    statusFilterChange: (state, action) => {
+      state.status = action.payload;
+    },
+    prioritiesFilterChange: (state, action) => {
+      state.priorities = action.payload;
+    },
+  },
+});
--- Channel F8-CoderX-HoleTex-NineDev/HoleTex/Redux and Redux Thunk/todo-app-redux/src/react-redux-toolkit/redux/store.js
+++ Channel F8-CoderX-HoleTex-NineDev/HoleTex/Redux and Redux Thunk/todo-app-redux/src/react-redux-toolkit/redux/store.js
@@ -1,9 +1,12 @@
-import { createStore } from "redux";
-import rootReducer from "./reducer";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
+import filtersReducer from "../components/Filters/FiltersSlice";
+import todoListReducer from "../components/TodoList/TodosSlice";
 
-const composedEnhancers = composeWithDevTools();
-
-const store = createStore(rootReducer, composedEnhancers);
+const store = configureStore({
+  reducer: {
+    filters: filtersReducer,
+    todoList: todoListReducer,
+  },
+});
 
 export default store;
