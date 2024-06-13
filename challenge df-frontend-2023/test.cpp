dcdf47c057f930a76ec4acc31e62f0dbed47f783
duynghiadev <duynghia22302@gmail.com>
2024-06-13 09:00:03 +0700

    test code c++

---
+++ challenge df-frontend-2023/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // Define two numbers to be added
+    int num1 = 5;
+    int num2 = 10;
+
+    // Calculate the sum of the numbers
+    int sum = num1 + num2;
+
+    // Print the sum to the console
+    std::cout << "The sum of " << num1 << " and " << num2 << " is " << sum << std::endl;
+
+    return 0;
+}
---
+++ challenge df-frontend-2023/test.h
@@ -0,0 +1,16 @@
+//
+// Created by Dell on 6/13/2024.
+//
+
+#ifndef TEST_H
+#define TEST_H
+
+
+
+class test {
+
+};
+
+
+
+#endif //TEST_H
