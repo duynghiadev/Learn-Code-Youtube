028fd3b4928111b2409a683eba5e95060377fd2e
duynghiadev <duynghia22302@gmail.com>
2024-06-15 08:57:21 +0700

    clean folder

--- vscode/channel Laptrinhlachuyennho-CodersX-DuocDev-WebStation/DuocDev/Next14/server/src/utils/helpers.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs'
-
-export const randomId = () => crypto.randomUUID().replace(/-/g, '')
-export const createFolder = (folderPath: string) => {
-  if (!fs.existsSync(folderPath)) {
-    fs.mkdirSync(folderPath, { recursive: true })
-  }
-}
