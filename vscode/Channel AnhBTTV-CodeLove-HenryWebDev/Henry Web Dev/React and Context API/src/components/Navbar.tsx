27689eee627282592a21cd89dc84e96252b21d4d
duynghiadev <duynghia22302@gmail.com>
2024-06-15 15:11:05 +0700

    React và Context API với TypeScript: Tập 1 - Props, useState và useEffect

--- vscode/Channel AnhBTTV-CodeLove-HenryWebDev/Henry Web Dev/React and Context API/src/components/Navbar.tsx
+++ vscode/Channel AnhBTTV-CodeLove-HenryWebDev/Henry Web Dev/React and Context API/src/components/Navbar.tsx
@@ -1,7 +1,43 @@
-import { AppBar, Box, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Box,
+  Button,
+  FormControl,
+  MenuItem,
+  Select,
+  Theme,
+  Toolbar,
+  Typography,
+  createStyles,
+  makeStyles,
+} from "@material-ui/core";
+import { ChangeEvent, useEffect, useState } from "react";
 import WelcomeMessage from "./WelcomeMessage";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    positionSelect: {
+      color: "white",
+      borderBottom: "1px solid white",
+    },
+  })
+);
+
 const Navbar = () => {
+  // styles
+  const classes = useStyles();
+
+  const [position, setPosition] = useState<string>("Full-stack Developer");
+  const [time, setTime] = useState<Date>(() => new Date(Date.now()));
+
+  useEffect(() => {
+    const timer = setInterval(() => setTime(new Date(Date.now())), 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const onPositionChange = (event: ChangeEvent<{ value: unknown }>) =>
+    setPosition(event.target.value as string);
+
   return (
     <AppBar position="static" color="primary">
       <Toolbar>
@@ -15,7 +51,33 @@ const Navbar = () => {
           <Typography variant="h6">My movies</Typography>
 
           <Box textAlign="center">
-            <WelcomeMessage position="Full-stack Developer" />
+            <WelcomeMessage position={position} />
+            <Box mt={1}>
+              <FormControl>
+                <Select
+                  value={position}
+                  onChange={onPositionChange}
+                  className={classes.positionSelect}
+                >
+                  <MenuItem value="Full-stack Developer">
+                    Full-stack Developer
+                  </MenuItem>
+                  <MenuItem value="Front-end Developer">
+                    Front-end Developer
+                  </MenuItem>
+                  <MenuItem value="Back-end Developer">
+                    Back-end Developer
+                  </MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+          </Box>
+
+          <Box textAlign="center">
+            <Box my={1}>
+              <Typography variant="h6">{time.toUTCString()}</Typography>
+            </Box>
+            <Button variant="contained">Login</Button>
           </Box>
         </Box>
       </Toolbar>
