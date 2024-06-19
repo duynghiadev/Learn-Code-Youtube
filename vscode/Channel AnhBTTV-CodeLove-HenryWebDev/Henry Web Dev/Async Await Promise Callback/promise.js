8c1dfaae638a314a075e7079586cc15a049fea6a
duynghiadev <duynghia22302@gmail.com>
2024-06-19 17:02:11 +0700

    JavaScript bất đồng bộ: Callback, Promise, Async/Await - Bài 4 - Giờ thì anh hứa để làm gì (Promise)

---
+++ vscode/Channel AnhBTTV-CodeLove-HenryWebDev/Henry Web Dev/Async Await Promise Callback/promise.js
@@ -0,0 +1,28 @@
+// ask for phone number
+const xinSoDt = () => {
+  console.log("Gọi điện cho thằng bạn. Cho tao xin số của thằng Nam");
+  console.log("Đang tìm...");
+
+  const thaoTacTimKiem = new Promise((resolve, reject) => {
+    let daTimThay;
+
+    setTimeout(() => {
+      daTimThay = true;
+      if (daTimThay) {
+        soDt = 123456;
+        console.log(`Đây rồi. Số của nó là ${soDt}`);
+        resolve(soDt);
+      } else {
+        reject("Tao không có số thằng Nam");
+      }
+    }, 1000);
+  });
+
+  return thaoTacTimKiem;
+};
+
+xinSoDt()
+  .then((soDt) => console.log(`Đã xin được số thằng Nam: ${soDt}`))
+  .catch((error) => console.log(error));
+
+// ask for phone number -> then, charge the phone baterry -> then, call
