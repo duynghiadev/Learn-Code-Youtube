df4734526f782c8ee668a40c03055368a3c63a8c
duynghiadev <duynghia22302@gmail.com>
2024-06-19 10:42:59 +0700

    JavaScript bất đồng bộ - Callback, Promise, Async/Await - Bài 3 - Gọi lại cho em nhé (Callback)

--- vscode/Channel AnhBTTV-CodeLove-HenryWebDev/Henry Web Dev/Async Await Promise Callback/callback.js
+++ vscode/Channel AnhBTTV-CodeLove-HenryWebDev/Henry Web Dev/Async Await Promise Callback/callback.js
@@ -17,21 +17,75 @@
 
 // console.log("=".repeat(40));
 
+//  =================================================
+
 // wrong solution
 
-const xinSoDt_1 = () => {
+// const xinSoDt_1 = () => {
+//   let soDt;
+//   console.log("Goi dien cho thang ban: Cho tao xin so thang Nam");
+//   console.log("Thang ban dang tim so thang Nam");
+
+//   setTimeout(() => {
+//     soDt = 123456;
+//     console.log("Da tim duoc so thang Nam");
+//   }, 1000);
+
+//   // bad solution
+//   setTimeout(() => {
+//     console.log("call number:", soDt);
+//   }, 2000);
+// };
+// xinSoDt_1();
+
+//  =================================================
+
+// using callback function
+
+// const xinSoDt_1 = (callback) => {
+//   let soDt;
+//   console.log("Goi dien cho thang ban: Cho tao xin so thang Nam");
+//   console.log("Thang ban dang tim so thang Nam");
+
+//   setTimeout(() => {
+//     soDt = 123456;
+//     console.log("Da tim duoc so thang Nam");
+//     callback(soDt);
+//   }, 1000);
+// };
+
+// const callback = (soDtDaNhan) =>
+//   console.log(`Day la so dien thoai thang Nam: ${soDtDaNhan}`);
+
+// xinSoDt_1(callback);
+
+//  =================================================
+
+// this is problem have many using callback function
+
+const xinSoDt_1 = (callback) => {
   let soDt;
   console.log("Goi dien cho thang ban: Cho tao xin so thang Nam");
   console.log("Thang ban dang tim so thang Nam");
 
   setTimeout(() => {
     soDt = 123456;
-    console.log("Da tim duoc so thang Nam");
+    console.log("Day roi. Da tim duoc so thang Nam");
+    callback(soDt, goiChoNam);
   }, 1000);
+};
 
-  // bad solution
+const sacPin = (soDth, hamGoiLaiSauKhiSacPin) => {
+  console.log("Doi ti. Sac pin da");
+
   setTimeout(() => {
-    console.log("call number:", soDt);
+    console.log("Sac pin xong. Bat dau goi cho Nam");
+    hamGoiLaiSauKhiSacPin(soDth);
   }, 2000);
 };
-xinSoDt_1();
+
+const goiChoNam = (soDt) =>
+  console.log(`Dang noi chuyen voi Nam sau khi goi vao so ${soDt}`);
+
+// proceed with the entire process
+xinSoDt_1(sacPin);
