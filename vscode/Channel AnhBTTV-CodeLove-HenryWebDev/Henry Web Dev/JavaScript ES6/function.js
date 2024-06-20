a4723247b0d27157b92d9e0ae2ec844b5cc37986
duynghiadev <duynghia22302@gmail.com>
2024-06-21 02:38:44 +0700

    Hướng dẫn JavaScript ES6 - Tập 2 - Hàm (Function)

---
+++ vscode/Channel AnhBTTV-CodeLove-HenryWebDev/Henry Web Dev/JavaScript ES6/function.js
@@ -0,0 +1,37 @@
+// ES5
+// function congHaiSo(a, b) {
+//   return a + b;
+// }
+// const tong = congHaiSo(1, 2);
+// console.log("tong:", tong);
+
+// ===========================================
+
+// ES6
+// const congHaiSo = (a, b) => {
+//   return a + b;
+// };
+// const tong = congHaiSo(1, 2);
+// console.log("tong:", tong);
+
+// ===========================================
+
+// const vietTen = (ten) => {
+//   console.log(ten);
+// };
+// vietTen("duynghiadev");
+
+// ===========================================
+
+// const vietTen = (ten) => console.log(ten);
+// vietTen("duynghiadev");
+
+// ===========================================
+
+// const nhanDoi = (x) => x * 2;
+// console.log("nhanDoi:", nhanDoi(3));
+
+// ===========================================
+// bai tap
+const congHaiSo = (x, y) => x + y;
+console.log("congHaiSo:", congHaiSo(2, 3));
