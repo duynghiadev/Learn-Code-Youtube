0839f59a76ed242a70019c55074abebc5ee95e3f
duynghiadev <duynghia22302@gmail.com>
2024-06-21 02:28:36 +0700

    Hướng dẫn JavaScript ES6 - Tập 1 - Biến (Variable)

---
+++ vscode/Channel AnhBTTV-CodeLove-HenryWebDev/Henry Web Dev/JavaScript ES6/variables.js
@@ -0,0 +1,43 @@
+// var anToi = "com";
+// var tienTrongVi = 100;
+
+// if (tienTrongVi > 10) var anToi = "pho";
+// console.log(anToi);
+
+// ===========================================
+
+// let anToi = "com";
+// let tienTrongVi = 100;
+
+// if (tienTrongVi > 10) {
+//   anToi = "pho";
+//   console.log("in if:", anToi);
+// }
+// console.log("out if:", anToi);
+
+// ===========================================
+
+// let anToi = "com";
+// let tienTrongVi = 100;
+
+// if (tienTrongVi > 10) {
+//   let anToi = "pho";
+//   console.log("in if:", anToi);
+// }
+// console.log("out if:", anToi);
+
+// ===========================================
+
+// const anToi = "com";
+// anToi = "pho";
+// console.log("anToi:", anToi);
+
+// ===========================================
+
+const buaAn = {
+  anTrua: "com",
+  anToi: "pho",
+};
+
+buaAn.anTrua = "bun";
+console.log("buaAn:", buaAn);
