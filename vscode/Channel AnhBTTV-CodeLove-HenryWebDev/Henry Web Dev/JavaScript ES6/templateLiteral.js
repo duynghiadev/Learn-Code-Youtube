ddc0f8d904c474e52fed3a2fe42804c2dfbf850d
duynghiadev <duynghia22302@gmail.com>
2024-06-21 02:49:06 +0700

    Hướng dẫn JavaScript ES6 - Tập 4 - Template Literals

---
+++ vscode/Channel AnhBTTV-CodeLove-HenryWebDev/Henry Web Dev/JavaScript ES6/templateLiteral.js
@@ -0,0 +1,14 @@
+// const ten = "Duynghiadev";
+// const ho = "Le";
+// console.log("Tên tôi là: " + ho + " " + ten);
+
+// ===========================================
+
+// const ten = "Duynghiadev";
+// const ho = "Le";
+// console.log(`Tên tôi là: ${ho} ${ten}`);
+
+// ===========================================
+// bai tap
+const age = 22;
+console.log(`Tôi năm nay ${age} tuổi`);
