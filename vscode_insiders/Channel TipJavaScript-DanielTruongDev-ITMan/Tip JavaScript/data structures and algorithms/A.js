31714f224e4345c7e64980b308972fb8227bd55c
duynghiadev <duynghia22302@gmail.com>
2024-07-21 13:55:44 +0700

    Bộ tài liệu cấu trúc dữ liệu mới và mẹo học tốt nhất giải thuật cho các anh chị tải xuống miễn phí !

--- vscode_insiders/Channel TipJavaScript-DanielTruongDev-ITMan/Tip JavaScript/data structures and algorithms/A.js
+++ vscode_insiders/Channel TipJavaScript-DanielTruongDev-ITMan/Tip JavaScript/data structures and algorithms/A.js
@@ -1,22 +1,22 @@
 // Programmer A uses a hash set to efficiently find common elements.
 
 function findCommonElements(arr1, arr2) {
-  const set1 = new Set(arr1)
-  const commonElements = []
+  const set1 = new Set(arr1);
+  const commonElements = [];
 
   for (const num of arr2) {
     if (set1.has(num)) {
-      commonElements.push(num)
+      commonElements.push(num);
     }
   }
-  return commonElements
+  return commonElements;
 }
 
-const ArrayA = [2, 5, 8, 10, 14]
-const ArrayB = [1, 6, 4, 8, 3]
+const ArrayA = [2, 5, 8, 10, 14];
+const ArrayB = [1, 6, 4, 8, 3];
 
-console.time('Programmer A')
-const resultA = findCommonElements(ArrayA, ArrayB)
-console.timeEnd('Programmer A')
+console.time("Programmer A");
+const resultA = findCommonElements(ArrayA, ArrayB);
+console.timeEnd("Programmer A");
 
-console.log('Common elements:', resultA)
+console.log("Common elements:", resultA);
--- vscode_insiders/Channel TipJavaScript-DanielTruongDev-ITMan/Tip JavaScript/data structures and algorithms/B.js
+++ vscode_insiders/Channel TipJavaScript-DanielTruongDev-ITMan/Tip JavaScript/data structures and algorithms/B.js
@@ -1,24 +1,24 @@
 // Programmer B is not familiar with efficient search techniques and uses a nested loop.
 
 function findCommonElements(arr1, arr2) {
-  const commonElements = []
+  const commonElements = [];
 
   for (const num1 of arr1) {
     for (const num2 of arr2) {
       if (num1 === num2) {
-        commonElements.push(num1)
-        break // Break to avoid duplicates
+        commonElements.push(num1);
+        break; // Break to avoid duplicates
       }
     }
   }
-  return commonElements
+  return commonElements;
 }
 
-const ArrayA = [2, 5, 8, 10, 14]
-const ArrayB = [1, 6, 4, 8, 3]
+const ArrayA = [2, 5, 8, 10, 14];
+const ArrayB = [1, 6, 4, 8, 3];
 
-console.time('Programmer B')
-const resultB = findCommonElements(ArrayA, ArrayB)
-console.timeEnd('Programmer B')
+console.time("Programmer B");
+const resultB = findCommonElements(ArrayA, ArrayB);
+console.timeEnd("Programmer B");
 
-console.log('Common elements:', resultB)
+console.log("Common elements:", resultB);
