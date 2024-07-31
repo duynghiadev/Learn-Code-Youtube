e201d5713f2d0290d30f2143386f942fa874de30
duynghiadev <duynghia22302@gmail.com>
2024-07-31 23:57:18 +0700

    clean code

---
+++ Learn-Flutter/Code On Sunday/your_name_price/android/app/src/main/kotlin/com/example/your_name_price/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.your_name_price
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- Learn-Flutter/Test_Flutter/android/app/src/main/kotlin/com/example/test_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.test_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
