6af9af97d197aff1d28b4c8e0dee9836dc1b107f
duynghiadev <duynghia22302@gmail.com>
2024-11-08 15:03:01 +0700

    Senior sử dụng async await fetch api thực sự pro nhu thế nào? | FETCH API

--- vscode_insiders/Channel TipJavaScript-DanielTruongDev-ITMan/Tip JavaScript/async-await-fetch/readme.md
+++ vscode_insiders/Channel TipJavaScript-DanielTruongDev-ITMan/Tip JavaScript/async-await-fetch/readme.md
@@ -1,3 +1,3 @@
 # Senior sử dụng async await fetch api thực sự pro nhu thế nào? | FETCH API
 
-[Link video](https://s.net.vn/dLvE)
+[Link video](https://www.youtube.com/watch?v=tf1ko6sBoLY)
--- vscode_insiders/Channel TipJavaScript-DanielTruongDev-ITMan/Tip JavaScript/async-await-fetch/script.js
+++ vscode_insiders/Channel TipJavaScript-DanielTruongDev-ITMan/Tip JavaScript/async-await-fetch/script.js
@@ -1,38 +1,40 @@
 const initApp = async () => {
-  const url = 'https://jsonplaceholder.typicode.com/todos/1'
+  const url = "https://jsonplaceholder.typicode.com/todos/1";
 
   // Level 2
   const getTodo = async () => {
-    return await fetch(url)
-  }
+    return await fetch(url);
+  };
 
   // Level 3
   const getTodo2 = async () => {
-    return (await fetch(url)).json()
-  }
+    return (await fetch(url)).json();
+  };
 
-  console.log('Init DOM')
+  console.log("Init DOM");
 
-  const btn_getList = document.querySelector('button')
+  const btn_getList = document.querySelector("button");
   if (btn_getList) {
-    btn_getList.addEventListener('click', async () => {
-      console.log('Event click')
-      // Cach 1 -> Level 1
+    btn_getList.addEventListener("click", async () => {
+      console.log("Event click");
+      // Cach 1 -> Level 1 -> cách này dành cho những người mới vào nghề
       fetch(url)
         .then((response) => response.json())
-        .then((data) => console.log('This is type code of Level 1 -> data:', data))
-        .catch((error) => console.log(error))
+        .then((data) =>
+          console.log("> This is type code of Level 1 -> data:", data)
+        )
+        .catch((error) => console.log(error));
 
-      // Cach 2 -> Level 2
-      const todo = await getTodo(url)
-      const data = await todo.json()
-      console.log('This is type code of Level 2 -> data:', data)
+      // Cach 2 -> Level 2 -> cách này dành cho người đi làm lâu năm
+      const todo = await getTodo(url);
+      const data = await todo.json();
+      console.log(">> This is type code of Level 2 -> data:", data);
 
-      // Cach 3 -> Level 3
-      const title = await getTodo2(url)
-      console.log('This is type code of Level 3 -> title:', title)
-    })
+      // Cach 3 -> Level 3 -> cách này dành cho cao thủ
+      const title = await getTodo2(url);
+      console.log(">>> This is type code of Level 3 -> title:", title);
+    });
   }
-}
+};
 
-document.addEventListener('DOMContentLoaded', initApp())
+document.addEventListener("DOMContentLoaded", initApp());
