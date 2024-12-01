558e45f64b097523ee35261f4439446a60417038
duynghiadev <duynghia22302@gmail.com>
2024-12-01 12:46:54 +0700

    clean code

--- react-native/Dao Quang/TodoList/to-do-list-app/src/constants/fontFamilies.ts
+++
@@ -1,6 +0,0 @@
-export const fontFamilies = {
-  regular: 'NunitoSans_10pt-Regular',
-  medium: 'NunitoSans_10pt-Medium',
-  semiBold: 'NunitoSans_10pt-SemiBold',
-  bold: 'NunitoSans_10pt-Bold',
-};
--- react-native/Nguyen Duc Hoang/React Native 2022/Readme.md
+++
@@ -1,3 +0,0 @@
-# Xây dựng ứng dụng Mobile với React Native 2022
-
-[Link](https://www.youtube.com/playlist?list=PLWBrqglnjNl12buYHtPhU3wjqk9rAuHhg)
